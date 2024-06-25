8afddcc017bb489c47323532d4c19968e1c28092
Minsung Kim <mskim4048@naver.com>
2024-06-25 15:40:15 +0900

    Initial Commit

---
+++ Offroad-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Offroad-iOS
+//
+//  Created by 김민성 on 2024/06/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
