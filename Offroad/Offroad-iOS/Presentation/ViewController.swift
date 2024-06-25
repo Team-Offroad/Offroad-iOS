3c44e16461a3e1cae289564c90b3007498433d24
Minsung Kim <mskim4048@naver.com>
2024-06-25 17:19:20 +0900

    [Setting] 폴더 구조 변경

--- Offroad/Offroad-iOS/Presentation/ViewController.swift
+++ Offroad/Offroad-iOS/Presentation/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .systemBlue
     }
 
 
