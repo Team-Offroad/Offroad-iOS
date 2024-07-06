ccfe1808a4ceda2440d03eca99705955a180df7a
Minsung Kim <mskim4048@naver.com>
2024-07-06 23:26:27 +0900

    [Add/#13] 색 Enum 타입들 추가

--- Offroad-iOS/Offroad-iOS/Global/Literals/UIColor+.swift
+++ Offroad-iOS/Offroad-iOS/Global/Literals/UIColor+.swift
@@ -5,4 +5,63 @@
 //  Created by 김민성 on 2024/07/06.
 //
 
-import Foundation
+import UIKit
+
+enum Primary: String {
+    case white = "#FFFFFF"
+    case black = "#000000"
+    case kakao = "#FEE500"
+    case error = "#F04452"
+    case mapGradi = "#5B5B5B"
+}
+
+enum Main: String {
+    // main1_75
+    case main175 = "#FEFBF6"
+    case main1 = "#FFF7E7"
+    case main2 = "#1D1E18"
+    case main3 = "#FFFBF4"
+}
+
+enum Sub: String {
+    case sub = "#FF901C"
+    case sub2 = "#E57A2C"
+    case sub3 = "#8B6546"
+    case sub4 = "#353028"
+}
+
+enum Grayscale: String {
+    case gray100 = "#D9D9D9"
+    case gray200 = "#BEBEBE"
+    case gray300 = "#B2B2B2"
+    case gray400 = "#7E7E7E"
+}
+
+enum Neutral: String {
+    case bottomBarButtonStroke = "#FEFBF6"
+    case btnInactive = "#EBEBEB"
+    case bottomBarInactive = "#A4A099"
+    case nametagInactive = "#FFEDDB"
+}
+
+enum BlackOpacity: String {
+    case black15 = "#000000"
+    case black25 = "#000000"
+    case black55 = "#000000"
+    case qrCamera = "#000000"
+}
+
+enum WhiteOpacity: String {
+    case white75 = "#FFFFFF"
+    case white25 = "#FFFFFF"
+}
+
+enum Home: String {
+    case homeBg = "#463E32"
+    case homeNametagStroke = "#FFE2C4"
+    case homeContents1 = "#8B8446"
+    case homeContents1GraphMain = "#FED761"
+    case homeContents1GraphSub = "#BBAC57"
+    case homeContents2 = "#E6CEAA"
+    case homeCharacterName = "#8B6546"
+}
