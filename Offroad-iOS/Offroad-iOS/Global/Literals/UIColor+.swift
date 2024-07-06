0ea6834c62d6002a5f81d5618f658c9c527d4310
Minsung Kim <mskim4048@naver.com>
2024-07-06 23:31:56 +0900

    [Add/#13] hexCode와 alpha 값으로 UIColor 초기화하는 편의생성자 확장 구현

--- Offroad-iOS/Offroad-iOS/Global/Literals/UIColor+.swift
+++ Offroad-iOS/Offroad-iOS/Global/Literals/UIColor+.swift
@@ -65,3 +65,27 @@ enum Home: String {
     case homeContents2 = "#E6CEAA"
     case homeCharacterName = "#8B6546"
 }
+
+
+extension UIColor {
+    
+    // 코드 출처: https://www.hackingwithswift.com/example-code/uicolor/how-to-convert-a-hex-color-to-a-uicolor
+    convenience init?(hexCode: String, alpha: CGFloat = 1.0) {
+        var hexFormatted: String = hexCode.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
+        
+        if hexFormatted.hasPrefix("#") {
+            hexFormatted = String(hexFormatted.dropFirst())
+        }
+        
+        guard hexFormatted.isValidHexSixDigits() else { return nil }
+        
+        var rgbValue: UInt64 = 0
+        Scanner(string: hexFormatted).scanHexInt64(&rgbValue)
+        
+        self.init(red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+                  green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+                  blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+                  alpha: alpha)
+    }
+    
+}
