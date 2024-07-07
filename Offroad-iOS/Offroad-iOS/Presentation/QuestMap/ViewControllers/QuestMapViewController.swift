f342d480a6c9f404b777b116463785158a7264a2
Minsung Kim <mskim4048@naver.com>
2024-07-07 21:34:50 +0900

    [Add/#15] QuestMapViewController.swift, QuestMapView.swift 파일 생성 및 폴더링

---
+++ Offroad-iOS/Offroad-iOS/Presentation/QuestMap/ViewControllers/QuestMapViewController.swift
@@ -0,0 +1,16 @@
+//
+//  QuestMapViewController.swift
+//  Offroad-iOS
+//
+//  Created by 김민성 on 2024/07/07.
+//
+
+import UIKit
+
+class QuestMapViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ Offroad-iOS/Offroad-iOS/Presentation/QuestMap/Views/QuestMapView.swift
@@ -0,0 +1,21 @@
+//
+//  QuestMapView.swift
+//  Offroad-iOS
+//
+//  Created by 김민성 on 2024/07/07.
+//
+
+import UIKit
+
+class QuestMapView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
