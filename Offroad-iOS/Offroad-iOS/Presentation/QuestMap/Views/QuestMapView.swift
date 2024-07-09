29f6e1248bca4db81065d89ddafa4aaf5d638caa
Minsung Kim <mskim4048@naver.com>
2024-07-09 11:58:37 +0900

    [Feat/#15] QuestMapView 기본 UI 구현

--- Offroad-iOS/Offroad-iOS/Presentation/QuestMap/Views/QuestMapView.swift
+++ Offroad-iOS/Offroad-iOS/Presentation/QuestMap/Views/QuestMapView.swift
@@ -7,15 +7,43 @@
 
 import UIKit
 
+import NMapsMap
+import SnapKit
+import Then
+
 class QuestMapView: UIView {
+    
+    let naverMapView = NMFNaverMapView()
+    let reloadLocationButton = UIButton()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
+        setupHierarchy()
+        setupLayout()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+
+extension QuestMapView {
+    
+    private func setupHierarchy() {
+        addSubviews(naverMapView)
+    }
+    
+    private func setupStyle() {
+        
+    }
+    
+    private func setupLayout() {
+        naverMapView.snp.makeConstraints { make in
+            make.edges.equalTo(safeAreaLayoutGuide.snp.edges)
+        }
     }
     
 }
