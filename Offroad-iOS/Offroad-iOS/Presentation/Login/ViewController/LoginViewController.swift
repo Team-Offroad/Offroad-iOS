83aa5786a4e0b71c16cee5279f33341b68b3d2e8
codeJiwon <loopy0107@sookmyung.ac.kr>
2024-07-09 23:48:32 +0900

    [Merge/#20] 머지 충돌 해결

---
+++ Offroad-iOS/Offroad-iOS/Presentation/Login/ViewController/LoginViewController.swift
@@ -0,0 +1,45 @@
+//
+//  LoginViewController.swift
+//  Offroad-iOS
+//
+//  Created by 조혜린 on 7/8/24.
+//
+
+import UIKit
+
+final class LoginViewController: UIViewController {
+    
+    //MARK: - Properties
+    
+    private let rootView = LoginView()
+    
+    // MARK: - Life Cycle
+    
+    override func loadView() {        
+        view = rootView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupTarget()
+    }
+}
+
+extension LoginViewController {
+    
+    // MARK: - Private Method
+    
+    private func setupTarget() {
+        rootView.setupKakaoLoginButton(action: kakaoLoginButtonTapped)
+        rootView.setupAppleLoginButton(action: appleLoginButtonTapped)
+    }
+    
+    private func kakaoLoginButtonTapped() {
+        print("kakaoLoginButtonTapped")
+    }
+    
+    private func appleLoginButtonTapped() {
+        print("appleLoginButtonTapped")
+    }
+}
---
+++ Offroad-iOS/Offroad-iOS/Presentation/Nickname/ViewController/NicknameViewController.swift
@@ -0,0 +1,46 @@
+//
+//  NicknameViewController.swift
+//  Offroad-iOS
+//
+//  Created by  정지원 on 7/9/24.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+class NicknameViewController: UIViewController {
+    private let nicknameView = NicknameView()
+    
+    //MARK: - Life Cycle
+    
+    override func loadView() {
+        view = nicknameView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.main(.main1)
+        
+        nicknameView.textField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
+    }
+
+}
+
+extension NicknameViewController {
+    
+    //MARK: - @objc
+    
+    @objc private func textFieldDidChange() {
+        let isTextFieldEmpty = nicknameView.textField.text?.isEmpty ?? true
+        if nicknameView.textField.isEnabled == true {
+            nicknameView.textField.layer.borderColor = UIColor.sub(.sub).cgColor
+        }
+        nicknameView.checkButton.isEnabled = !isTextFieldEmpty
+        nicknameView.checkButton.setTitleColor(isTextFieldEmpty ? UIColor.grayscale(.gray100) : UIColor.primary(.white), for: .normal)
+        nicknameView.checkButton.backgroundColor = isTextFieldEmpty ? UIColor.main(.main3) : UIColor.primary(.black)
+    }
+}
+
