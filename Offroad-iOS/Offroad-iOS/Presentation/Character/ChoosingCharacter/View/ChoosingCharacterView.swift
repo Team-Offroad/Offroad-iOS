b164ee5582d2cdcd6a9c413f0e4f44208214b900
codeJiwon <loopy0107@sookmyung.ac.kr>
2024-07-10 19:41:55 +0900

    [Refactor/#25] View와 ViewController 책임 분리

--- Offroad-iOS/Offroad-iOS/Presentation/Character/ChoosingCharacter/View/ChoosingCharacterView.swift
+++ Offroad-iOS/Offroad-iOS/Presentation/Character/ChoosingCharacter/View/ChoosingCharacterView.swift
@@ -5,4 +5,97 @@
 //  Created by  정지원 on 7/10/24.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+import Then
+
+final class ChoosingCharacterView: UIView {
+    
+    //MARK: - Properties
+    
+    let images = ["img_human", "img_red_hair", "img_naked_human"]
+    let captions = ["오프", "로드", "근원"]
+    
+    var extendedImages: [String] {
+        var extended = images
+        extended.insert(images.last!, at: 0)
+        extended.append(images.first!)
+        return extended
+    }
+    
+    let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 0
+        layout.minimumInteritemSpacing = 0
+        layout.itemSize = CGSize(width: UIScreen.main.bounds.width, height: 300)
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.isPagingEnabled = true
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.backgroundColor = UIColor.sub(.sub)
+        return collectionView
+    }()
+    
+    let pageControl = UIPageControl().then {
+        $0.currentPage = 0
+        $0.pageIndicatorTintColor = .gray
+        $0.currentPageIndicatorTintColor = .black
+    }
+    
+    let captionLabel = UILabel().then {
+        $0.textAlignment = .center
+        $0.text = "키티"
+    }
+    
+    // MARK: - Life Cycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupHierarchy()
+        setupStyle()
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension ChoosingCharacterView {
+    
+    // MARK: - Private Func
+    
+    private func setupHierarchy() {
+        addSubviews(
+            collectionView,
+            pageControl,
+            captionLabel
+        )
+    }
+    
+    private func setupStyle() {
+        backgroundColor = UIColor.main(.main1)
+    }
+    
+    private func setupLayout() {
+        
+        collectionView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview()
+            make.height.equalTo(300)
+        }
+        
+        pageControl.numberOfPages = images.count
+        pageControl.snp.makeConstraints { make in
+            make.top.equalTo(collectionView.snp.bottom).offset(10)
+            make.centerX.equalToSuperview()
+        }
+        
+        captionLabel.snp.makeConstraints { make in
+            make.top.equalTo(pageControl.snp.bottom).offset(10)
+            make.leading.trailing.equalToSuperview().inset(20)
+        }
+    }
+}
+
--- Offroad-iOS/Offroad-iOS/Presentation/Character/ChoosingCharacter/ViewController/ChoosingCharacterViewController.swift
+++ Offroad-iOS/Offroad-iOS/Presentation/Character/ChoosingCharacter/ViewController/ChoosingCharacterViewController.swift
@@ -5,4 +5,93 @@
 //  Created by  정지원 on 7/10/24.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+import Then
+
+class ChoosingCharacterViewController: UIViewController {
+    
+    //MARK: - Properties
+    
+    private let choosingCharacterView = ChoosingCharacterView()
+    
+    //MARK: - Life Cycle
+    
+    override func loadView() {
+        view = choosingCharacterView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.main(.main1)
+        
+        setupCollectionView()
+        
+        DispatchQueue.main.async {
+            self.choosingCharacterView.collectionView.scrollToItem(at: IndexPath(item: 1, section: 0), at: .centeredHorizontally, animated: false)
+        }
+    }
+    
+    //MARK: - Private Method
+    
+    private func setupCollectionView() {
+        choosingCharacterView.collectionView.delegate = self
+        choosingCharacterView.collectionView.dataSource = self
+        choosingCharacterView.collectionView.register(CarouselCell.self, forCellWithReuseIdentifier: CarouselCell.identifier)
+    }
+}
+
+extension ChoosingCharacterViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return choosingCharacterView.extendedImages.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CarouselCell.identifier, for: indexPath) as! CarouselCell
+        cell.configure(imageName: choosingCharacterView.extendedImages[indexPath.item])
+        return cell
+    }
+
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let pageIndex = Int(scrollView.contentOffset.x / view.frame.width)
+        choosingCharacterView.pageControl.currentPage = (pageIndex - 1 + choosingCharacterView.images.count) % choosingCharacterView.images.count
+        choosingCharacterView.captionLabel.text = choosingCharacterView.captions[choosingCharacterView.pageControl.currentPage]
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let pageWidth = scrollView.frame.width
+        let currentPage = Int(scrollView.contentOffset.x / pageWidth)
+        
+        if currentPage == 0 {
+            choosingCharacterView.collectionView.scrollToItem(at: IndexPath(item: choosingCharacterView.images.count, section: 0), at: .centeredHorizontally, animated: false)
+        } else if currentPage == choosingCharacterView.extendedImages.count - 1 {
+            choosingCharacterView.collectionView.scrollToItem(at: IndexPath(item: 1, section: 0), at: .centeredHorizontally, animated: false)
+        }
+    }
+}
+
+class CarouselCell: UICollectionViewCell {
+    static let identifier = "CarouselCell"
+
+    private let imageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFit
+        $0.clipsToBounds = true
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(imageView)
+        imageView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func configure(imageName: String) {
+        imageView.image = UIImage(named: imageName)
+    }
+}
+
