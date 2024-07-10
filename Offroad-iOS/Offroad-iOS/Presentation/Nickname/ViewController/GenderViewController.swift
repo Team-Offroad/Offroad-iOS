808930fdc8b721d905fecb2042a10766c09f4f7c
codeJiwon <loopy0107@sookmyung.ac.kr>
2024-07-10 14:58:22 +0900

    [Feat/#25] BirthView의 labels, buttons 생성

---
+++ Offroad-iOS/Offroad-iOS/Presentation/Nickname/ViewController/GenderViewController.swift
@@ -0,0 +1,15 @@
+//
+//  GenderViewController.swift
+//  Offroad-iOS
+//
+//  Created by  정지원 on 7/10/24.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class GenderViewController: UIViewController {
+    
+}
