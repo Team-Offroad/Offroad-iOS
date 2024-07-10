798b4d3eedaeb856b666deaec89cd09a77983998
codeJiwon <loopy0107@sookmyung.ac.kr>
2024-07-10 18:56:27 +0900

    [Add/#25] 캐릭터 이미지 추가 및 파일 생성

---
+++ Offroad-iOS/Offroad-iOS/Presentation/Character/ChoosingCharacter/View/ChoosingCharacterView.swift
@@ -0,0 +1,8 @@
+//
+//  ChoosingCharacterView.swift
+//  Offroad-iOS
+//
+//  Created by  정지원 on 7/10/24.
+//
+
+import Foundation
---
+++ Offroad-iOS/Offroad-iOS/Presentation/Character/ChoosingCharacter/ViewController/ChoosingCharacterViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ChoosingCharacterViewController.swift
+//  Offroad-iOS
+//
+//  Created by  정지원 on 7/10/24.
+//
+
+import Foundation
