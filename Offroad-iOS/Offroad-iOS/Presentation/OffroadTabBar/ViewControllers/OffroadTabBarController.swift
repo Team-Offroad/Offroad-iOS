2550ff4a488bf989c51a309c8afbcbbbc4eecf9b
Minsung Kim <mskim4048@naver.com>
2024-07-12 11:56:26 +0900

    [Add/#40] OffroadTabBarController.swift 파일 추가 및 관련 폴더 추가

---
+++ Offroad-iOS/Offroad-iOS/Presentation/OffroadTabBar/ViewControllers/OffroadTabBarController.swift
@@ -0,0 +1,43 @@
+//
+//  OffroadTabBarController.swift
+//  Offroad-iOS
+//
+//  Created by 김민성 on 2024/07/12.
+//
+
+import UIKit
+
+class OffroadTabBarController: UITabBarController {
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - UI Properties
+    
+    
+    // MARK: - Life Cycle
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
+
+
+
+extension OffroadTabBarController {
+    
+    // MARK: - Layout
+    
+    
+    // MARK: - @objc
+    
+    
+    // MARK: - Private Func
+    
+    
+    // MARK: - Func
+        
+}
