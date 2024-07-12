58f3784e711a2726fec221c357ae74f294b8691e
Minsung Kim <mskim4048@naver.com>
2024-07-12 18:11:42 +0900

    [Feat/#40] OffroadTabBarController 기본 UI 코드 작성

--- Offroad-iOS/Offroad-iOS/Presentation/OffroadTabBar/ViewControllers/OffroadTabBarController.swift
+++ Offroad-iOS/Offroad-iOS/Presentation/OffroadTabBar/ViewControllers/OffroadTabBarController.swift
@@ -10,7 +10,7 @@ import UIKit
 class OffroadTabBarController: UITabBarController {
     
     // MARK: - Properties
-    
+    var originalTabBarHeight: CGFloat = 0
     
     // MARK: - UI Properties
     
@@ -21,23 +21,71 @@ class OffroadTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupHierarchy()
+        setupLayout()
+        setupStyle()
+        setOffroadViewControllers()
+        setTabBarButtons()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        print("탭바컨트롤러:", #function)
+        super.viewDidLayoutSubviews()
         
+        originalTabBarHeight = tabBar.frame.height
+        
+        let tabBarHeightFromBottomEdge: CGFloat = 96
+        var newFrame = tabBar.frame
+        newFrame.size.height = tabBarHeightFromBottomEdge
+        newFrame.origin.y = view.frame.size.height - tabBarHeightFromBottomEdge
+        tabBar.frame = newFrame
     }
 }
 
-
-
 extension OffroadTabBarController {
     
-    // MARK: - Layout
     
+    // MARK: - Layout
+    private func setupHierarchy() {
+        
+    }
+    
+    private func setupLayout() {
+        
+    }
+    
+    private func setupStyle() {
+        tabBar.clipsToBounds = false
+        tabBar.backgroundColor = .sub(.sub4)
+        
+        tabBar.tintColor = .main(.main1)
+        tabBar.unselectedItemTintColor = .main(.main175)
+    }
+    
+    private func setOffroadViewControllers() {
+        let viewControllersArray: [UIViewController] = [
+            HomeViewController(),
+            QuestMapNavigationController(rootViewController: QuestMapViewController()),
+            LoginViewController()
+        ]
+        
+        setViewControllers(viewControllersArray, animated: false)
+        selectedIndex = 1
+    }
+    
+    private func setTabBarButtons() {
+        tabBar.items?[0].image = UIImage.icnHome
+        tabBar.items?[0].title = "Home"
+        tabBar.items?[1].image = UIImage.icnTabBarOffroadLogo
+        tabBar.items?[1].title = "Quest"
+        tabBar.items?[2].image = UIImage.icnPerson
+        tabBar.items?[2].title = "My"
+    }
     
     // MARK: - @objc
     
-    
     // MARK: - Private Func
-    
-    
+        
     // MARK: - Func
         
 }
