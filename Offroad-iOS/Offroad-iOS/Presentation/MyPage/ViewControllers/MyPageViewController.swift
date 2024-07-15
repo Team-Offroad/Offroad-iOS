d9786d4f85ea9eee5f407b54c5550ad31e1ad6de
Minsung Kim <mskim4048@naver.com>
2024-07-15 18:24:05 +0900

    [Add/#44] TabBarController의 세번째 뷰컨트롤러로 넣을 MyPageViewController 임시 생성

---
+++ Offroad-iOS/Offroad-iOS/Presentation/MyPage/ViewControllers/MyPageViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MyPageViewController.swift
+//  Offroad-iOS
+//
+//  Created by 김민성 on 2024/07/15.
+//
+
+import UIKit
+
+final class MyPageViewController: UIViewController {
+    
+    //MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .primary(.white)
+    }
+    
+}
