6d31c7ff2a036ffcf1f5d155283e3f536bcb077a
codeJiwon <loopy0107@sookmyung.ac.kr>
2024-07-16 00:49:34 +0900

    [Del/#29] 중복된 코드 삭제

--- Offroad-iOS/Offroad-iOS/Presentation/Splash/ViewController/SplashViewController.swift
+++ Offroad-iOS/Offroad-iOS/Presentation/Splash/ViewController/SplashViewController.swift
@@ -53,32 +53,4 @@ extension SplashViewController {
         present(loginViewController, animated: false, completion: nil)
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.6) {
-            self.rootView.dismissOffroadLogiView {
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
-                    self.presentLoginViewController()
-                }
-            }
-        }
-    }
-}
-
-extension SplashViewController {
-    
-    //MARK: - Private Func
-    
-    private func presentLoginViewController() {
-        let loginViewController = LoginViewController()
-        loginViewController.modalPresentationStyle = .fullScreen
-        
-        let transition = CATransition()
-        transition.duration = 0.6
-        transition.type = .fade
-        transition.subtype = .fromRight
-        view.window?.layer.add(transition, forKey: kCATransition)
-
-        
-        present(loginViewController, animated: false, completion: nil)
-    }
 }
