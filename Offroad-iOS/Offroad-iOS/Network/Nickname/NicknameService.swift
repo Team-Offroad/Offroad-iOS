abbba8a7a05912d2c28781851273df8c886b4a03
codeJiwon <loopy0107@sookmyung.ac.kr>
2024-07-16 18:17:58 +0900

    [Feat/#61] NicknameService 파일 생성

--- Offroad-iOS/Offroad-iOS/Network/Nickname/NicknameService.swift
+++ Offroad-iOS/Offroad-iOS/Network/Nickname/NicknameService.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+import Moya
+
+protocol NicknameServiceProtocol {
+    func checkNicknameDuplicate(completion: @escaping (NetworkResult<NicknameCheckResponseDTO>) -> ())
+}
+
+final class NicknameService: BaseService, NicknameServiceProtocol {
+    func checkNicknameDuplicate(completion: @escaping (NetworkResult<NicknameCheckResponseDTO>) -> ()) {
+        <#code#>
+    }
+    
+    let provider = MoyaProvider<NicknameAPI>(plugins: [MoyaPlugin()])
+
+    func getAdventureInfo(completion: @escaping (NetworkResult<NicknameCheckResponseDTO>) -> ()) {
+        provider.request(.checkNicknameDuplicate) { result in
+            switch result {
+            case .success(let response):
+                let networkResult: NetworkResult<NicknameCheckResponseDTO> = self.fetchNetworkResult(
+                    statusCode: response.statusCode,
+                    data: response.data
+                )
+                completion(networkResult)
+            case .failure(let err):
+                print(err)
+            }
+        }
+    }
+}
+
