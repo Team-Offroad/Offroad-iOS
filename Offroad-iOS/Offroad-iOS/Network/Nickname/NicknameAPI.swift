e6d9bf18ec41a64f66f0a8a1509c6db2822a1988
codeJiwon <loopy0107@sookmyung.ac.kr>
2024-07-16 17:40:59 +0900

    [Feat/#61] NicknameAPI 파일 생성

--- Offroad-iOS/Offroad-iOS/Network/Nickname/NicknameAPI.swift
+++ Offroad-iOS/Offroad-iOS/Network/Nickname/NicknameAPI.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+import Moya
+
+enum NicknameAPI {
+    case checkNicknameDuplicate
+}
+
+extension NicknameAPI: BaseTargetType {
+
+    var headerType: HeaderType { return .accessTokenHeaderForGet }
+        
+    var path: String {
+        switch self {
+        case .checkNicknameDuplicate:
+            return "/users/nickname/check"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .checkNicknameDuplicate:
+            return .get
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .checkNicknameDuplicate:
+            return .requestParameters(parameters: ["nickname": "정지원"], encoding: URLEncoding.queryString)
+        }
+    }
+}
+
---
+++ Offroad-iOS/Offroad-iOS/Presentation/Nickname/Model/NicknameModel.swift
@@ -0,0 +1,8 @@
+//
+//  NicknameModel.swift
+//  Offroad-iOS
+//
+//  Created by  정지원 on 7/16/24.
+//
+
+import Foundation
