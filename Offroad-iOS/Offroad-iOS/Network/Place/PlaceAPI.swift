cd472b013735854ddd06209ca3edc9f062967c7f
Minsung Kim <mskim4048@naver.com>
2024-07-17 02:42:37 +0900

    [Feat/#63] 등록 장소 조회 기본 네트워크 통신 코드 작성(DTO, API, Service)

--- Offroad-iOS/Offroad-iOS/Network/Place/PlaceAPI.swift
+++ Offroad-iOS/Offroad-iOS/Network/Place/PlaceAPI.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+
+import Moya
+import NMapsMap
+
+enum PlaceAPI {
+    case getRegisteredPlaces(requestDTO: RegisteredPlaceRequestDTO)
+}
+
+extension PlaceAPI: BaseTargetType {
+
+    var headerType: HeaderType { return .noneHeader }
+    
+    var parameter: [String : Any]? {
+        switch self {
+        case .getRegisteredPlaces:
+            return .none
+        }
+    }
+    
+    var path: String {
+        switch self {
+        case .getRegisteredPlaces:
+            return "/places"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .getRegisteredPlaces:
+            return .get
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .getRegisteredPlaces(let requestDTO):
+            return .requestJSONEncodable(requestDTO)
+        }
+    }
+}
--- Offroad-iOS/Offroad-iOS/Network/Place/PlaceService.swift
+++ Offroad-iOS/Offroad-iOS/Network/Place/PlaceService.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+import Moya
+
+protocol RegisteredPlaceServiceProtocol {
+    func getAdventureInfo(
+        requestDTO: RegisteredPlaceRequestDTO,
+        completion: @escaping (NetworkResult<RegisteredPlaceResponseDTO>) -> ()
+    )
+}
+
+final class RegisteredPlaceService: BaseService, RegisteredPlaceServiceProtocol {
+    let provider = MoyaProvider<PlaceAPI>(plugins: [MoyaPlugin()])
+
+    func getAdventureInfo(
+        requestDTO: RegisteredPlaceRequestDTO,
+        completion: @escaping (NetworkResult<RegisteredPlaceResponseDTO>) -> ()
+    ) {
+        provider.request(.getRegisteredPlaces(requestDTO: requestDTO)) { result in
+            switch result {
+            case .success(let response):
+                let networkResult: NetworkResult<RegisteredPlaceResponseDTO> = self.fetchNetworkResult(
+                    statusCode: response.statusCode,
+                    data: response.data
+                )
+                completion(networkResult)
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
