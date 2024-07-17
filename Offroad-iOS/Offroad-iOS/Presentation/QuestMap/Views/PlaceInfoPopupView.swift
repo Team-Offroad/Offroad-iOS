804ce788b4ec821ffa45821130342c270ca94e6d
Minsung Kim <mskim4048@naver.com>
2024-07-17 09:15:23 +0900

    [Feat/#63] PlaceInfoPopupView 팝업뷰 기본 UI 설정

--- Offroad-iOS/Offroad-iOS/Presentation/QuestMap/Views/PlaceInfoPopupView.swift
+++ Offroad-iOS/Offroad-iOS/Presentation/QuestMap/Views/PlaceInfoPopupView.swift
@@ -13,8 +13,11 @@ class PlaceInfoPopupView: UIView {
     
     let popupView = UIView()
     
+    let offroadLogoImageView = UIImageView(image: .icnOffroadlogoPopupView)
     let nameLabel = UILabel()
-    let placeCategoryImage: UIImage? = nil
+    let nameAndImageStackView = UIStackView()
+    
+    let placeCategoryImageView = UIImageView()
     let shortDescriptionLabel = UILabel()
     let addresssLabel = UILabel()
     let visitCountLabel = UILabel()
@@ -41,8 +44,11 @@ class PlaceInfoPopupView: UIView {
 extension PlaceInfoPopupView {
     
     private func setupHierarchy() {
+        nameAndImageStackView.addArrangedSubviews(nameLabel, placeCategoryImageView)
+        
         popupView.addSubviews(
-            nameLabel,
+            offroadLogoImageView,
+            nameAndImageStackView,
             shortDescriptionLabel,
             addresssLabel,
             visitCountLabel,
@@ -53,10 +59,86 @@ extension PlaceInfoPopupView {
     
     private func setupStyle() {
         popupView.backgroundColor = .main(.main3)
+        
+        nameLabel.do { label in
+            label.font = .offroad(style: .iosTooltipTitle)
+            label.textColor = .main(.main2)
+            label.textAlignment = .left
+        }
+        
+        nameAndImageStackView.do { stackView in
+            stackView.axis = .horizontal
+            stackView.spacing = 5
+            stackView.alignment = .center
+            stackView.distribution = .fillProportionally
+        }
+        
+        shortDescriptionLabel.do { label in
+            label.font = .offroad(style: .iosTextContents)
+            label.textColor = .main(.main2)
+            label.textAlignment = .left
+        }
+        
+        addresssLabel.do { label in
+            label.font = .offroad(style: .iosTextContentsSmall)
+            label.textColor = .grayscale(.gray400)
+            label.textAlignment = .left
+        }
+        
+        visitCountLabel.do { label in
+            label.font = .offroad(style: .iosTooltipNumber)
+            label.textColor = .sub(.sub2)
+            label.textAlignment = .left
+        }
+        
+        exploreButton.do { button in
+            button.setTitle("탐험하기", for: .normal)
+            button.setImage(.btnPopupClose, for: .normal)
+            button.titleLabel?.font = .offroad(style: .iosBtnSmall)
+            button.setTitleColor(.primary(.white), for: .normal)
+            button.backgroundColor = .sub(.sub)
+            button.roundCorners(cornerRadius: 5)
+        }
     }
     
     private func setupLayout() {
         
+        nameAndImageStackView.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(17)
+            make.leading.equalToSuperview().inset(15)
+        }
+        
+        shortDescriptionLabel.snp.makeConstraints { make in
+            make.top.equalTo(nameAndImageStackView.snp.bottom).offset(10)
+            make.leading.equalToSuperview().inset(15)
+        }
+        
+        addresssLabel.snp.makeConstraints { make in
+            make.top.equalTo(shortDescriptionLabel.snp.bottom).offset(4)
+            make.leading.equalToSuperview().inset(15)
+            
+        }
+        
+        visitCountLabel.snp.makeConstraints { make in
+            make.top.equalTo(addresssLabel.snp.bottom).offset(10)
+            make.leading.equalToSuperview().inset(15)
+        }
+        
+        exploreButton.snp.makeConstraints { make in
+            make.top.equalTo(visitCountLabel.snp.bottom).offset(15)
+            make.horizontalEdges.equalToSuperview().inset(14)
+            make.bottom.equalToSuperview().inset(14)
+        }
+        
+        closeButton.snp.makeConstraints { make in
+            make.top.trailing.equalToSuperview()
+            make.width.height.equalTo(48)
+        }
+        
+        
+        popupView.snp.makeConstraints { make in
+            make.centerX.centerY.equalToSuperview()
+        }
     }
     
 }
