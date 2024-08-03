251bf9c6c2c9d469667b465d258c9aeaa7c01ff2
Minsung Kim <mskim4048@naver.com>
2024-08-03 14:12:37 +0900

    [Add/#123] PlaceListViewController.swift, PlaceListView.swift 파일 추가

---
+++ Offroad-iOS/Offroad-iOS/Presentation/PlaceList/ViewControllers/PlaceListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PlaceListViewController.swift
+//  Offroad-iOS
+//
+//  Created by 김민성 on 8/3/24.
+//
+
+import UIKit
+
+class PlaceListViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ Offroad-iOS/Offroad-iOS/Presentation/PlaceList/Views/PlaceListView.swift
@@ -0,0 +1,14 @@
+//
+//  PlaceListView.swift
+//  Offroad-iOS
+//
+//  Created by 김민성 on 8/3/24.
+//
+
+import UIKit
+
+class PlaceListView: UIView {
+    
+    
+    
+}
