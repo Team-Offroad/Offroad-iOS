17ea70f8730dabd112bcfe301400889b57a75bfc
codeJiwon <loopy0107@sookmyung.ac.kr>
2024-08-13 16:03:38 +0900

    [Refactor/#133] View와 ViewController 책임 분리

--- Offroad-iOS/Offroad-iOS/Presentation/MyPage/Character/View/AcquiredCharactersView.swift
+++ Offroad-iOS/Offroad-iOS/Presentation/MyPage/Character/View/AcquiredCharactersView.swift
@@ -9,5 +9,60 @@ import UIKit
 import SnapKit
 
 class AcquiredCharactersView: UIView {
-    
+
+    // MARK: - Properties
+
+    private lazy var layout: UICollectionViewFlowLayout = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        let padding: CGFloat = 20
+        layout.itemSize = CGSize(width: 162, height: 214)
+        layout.minimumLineSpacing = padding
+        layout.minimumInteritemSpacing = padding
+        return layout
+    }()
+
+    lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(AcquiredCharactersCell.self, forCellWithReuseIdentifier: "AcquiredCharactersCell")
+        collectionView.backgroundColor = .clear
+        return collectionView
+    }()
+
+    // MARK: - Life Cycle
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupStyle()
+        setupHierarchy()
+        setupLayout()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Private Functions
+
+    private func setupStyle() {
+        backgroundColor = UIColor.main(.main3)
+    }
+
+    private func setupHierarchy() {
+        addSubview(collectionView)
+    }
+
+    private func setupLayout() {
+        let totalCellWidth = 2 * layout.itemSize.width
+        let totalSpacingWidth = layout.minimumInteritemSpacing
+        let collectionViewWidth = totalCellWidth + totalSpacingWidth
+
+        collectionView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview() // Center horizontally
+            make.top.equalToSuperview().offset(20)
+            make.bottom.equalToSuperview().offset(-20)
+            make.width.equalTo(collectionViewWidth)
+        }
+    }
 }
+
--- Offroad-iOS/Offroad-iOS/Presentation/MyPage/Character/ViewController/AcquiredCharactersViewController.swift
+++ Offroad-iOS/Offroad-iOS/Presentation/MyPage/Character/ViewController/AcquiredCharactersViewController.swift
@@ -4,58 +4,37 @@
 //
 //  Created by  정지원 on 8/11/24.
 //
-
 import UIKit
-import SnapKit
 
 class AcquiredCharactersViewController: UIViewController {
-    var collectionView: UICollectionView!
-    
+
+    // MARK: - Properties
+
+    private let acquiredCharactersView = AcquiredCharactersView()
+
+    // MARK: - Life Cycle
+
+    override func loadView() {
+        self.view = acquiredCharactersView
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureUI()
+        setupDelegate()
     }
-    
-    private func configureUI() {
-        view.backgroundColor = UIColor.white // Setting the background color to match the screenshot
-        setupCollectionView()
-    }
-}
 
-extension AcquiredCharactersViewController {
-    func setupCollectionView() {
-        let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .vertical
-        let padding: CGFloat = 20 // Updated padding as needed
-        layout.itemSize = CGSize(width: 162, height: 214)  // Ensure this matches cell constraints
-        layout.minimumLineSpacing = padding
-        layout.minimumInteritemSpacing = padding
-        
-        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        collectionView.register(AcquiredCharactersCell.self, forCellWithReuseIdentifier: "AcquiredCharactersCell")
-        collectionView.backgroundColor = .clear
-        collectionView.delegate = self
-        collectionView.dataSource = self
-        view.addSubview(collectionView)
-        
-        // Calculate the total width used by cells and their spacing
-        let totalCellWidth = 2 * layout.itemSize.width
-        let totalSpacingWidth = layout.minimumInteritemSpacing
-        
-        // Calculate the collectionView width including spacing
-        let collectionViewWidth = totalCellWidth + totalSpacingWidth
-        
-        // Center the collectionView in its superview
-        collectionView.snp.makeConstraints { make in
-            make.centerX.equalToSuperview() // Center horizontally
-            make.top.equalToSuperview().offset(20)
-            make.bottom.equalToSuperview().offset(-20)
-            make.width.equalTo(collectionViewWidth) // Set the width of the collectionView
-        }
+    // MARK: - Private Func
+
+    private func setupDelegate() {
+        acquiredCharactersView.collectionView.delegate = self
+        acquiredCharactersView.collectionView.dataSource = self
     }
 }
 
 extension AcquiredCharactersViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    // MARK: - CollectionView Func
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 10 // Adjust based on your data
     }
