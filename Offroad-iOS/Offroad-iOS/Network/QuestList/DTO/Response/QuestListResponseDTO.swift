857aa1383ba594c3ee8148351bb4537773e7816c
Minsung Kim <mskim4048@naver.com>
2024-08-27 22:57:41 +0900

    [Feat/#145] QuestListResponseDTO 구현

--- Offroad-iOS/Offroad-iOS/Network/QuestList/DTO/Response/QuestListResponseDTO.swift
+++ Offroad-iOS/Offroad-iOS/Network/QuestList/DTO/Response/QuestListResponseDTO.swift
@@ -8,5 +8,19 @@
 import Foundation
 
 struct QuestListResponseDTO: Codable {
-    
+    let message: String
+    let data: QuestListData
+}
+
+struct QuestListData: Codable {
+    let questList: [Quest]
+}
+
+struct Quest: Codable {
+    let questName: String
+    let description: String
+    let currentCount: Int
+    let totalCount: Int
+    let requirement: String
+    let reward: String
 }
