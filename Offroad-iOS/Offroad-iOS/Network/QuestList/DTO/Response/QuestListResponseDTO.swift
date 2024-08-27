e63622df3a03a120b290a11b1236eba2c30c55a4
Minsung Kim <mskim4048@naver.com>
2024-08-27 22:21:25 +0900

    [Feat/#145] 퀘스트 목록 서버 통신 파일 생성 및 API 기본 구현

---
+++ Offroad-iOS/Offroad-iOS/Network/QuestList/DTO/Response/QuestListResponseDTO.swift
@@ -0,0 +1,12 @@
+//
+//  QuestListResponseDTO.swift
+//  Offroad-iOS
+//
+//  Created by 김민성 on 8/27/24.
+//
+
+import Foundation
+
+struct QuestListResponseDTO: Codable {
+    
+}
---
+++ Offroad-iOS/Offroad-iOS/Network/QuestList/QuestListService.swift
@@ -0,0 +1,8 @@
+//
+//  QuestListService.swift
+//  Offroad-iOS
+//
+//  Created by 김민성 on 8/27/24.
+//
+
+import Foundation
