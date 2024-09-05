0291359ec31e649ae94d2c07c076ad818e9a5059
JoHyerin <gpfls468@gmail.com>
2024-09-06 01:20:48 +0900

    [Feat/#160] patchMarketingConsent API 연결 (설정 내 마케팅 수신 동의 탭)

--- Offroad-iOS/Offroad-iOS/Presentation/MyPage/Settings/MarketingConsent/ViewController/MarketingConsentViewController.swift
+++ Offroad-iOS/Offroad-iOS/Presentation/MyPage/Settings/MarketingConsent/ViewController/MarketingConsentViewController.swift
@@ -45,9 +45,22 @@ extension MarketingConsentViewController {
         rootView.disagreeButton.addTarget(self, action: #selector(disagreeButtonTapped), for: .touchUpInside)
     }
     
+    private func patchMarketingConsent(isAgreed: MarketingConsentRequestDTO) {
+        NetworkService.shared.profileService.patchMarketingConsent(body: isAgreed) { response in
+            switch response {
+            case .success(let data):
+                print("마케팅 수신 여부 변경 성공!\n요청 성공 값: \(isAgreed.marketing)")
+            default:
+                break
+            }
+        }
+    }
+    
     //MARK: - @Objc Func
     
     @objc private func agreeButtonTapped() {
+        patchMarketingConsent(isAgreed: MarketingConsentRequestDTO(marketing: true))
+        
         rootView.dismissPopupView()
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.4){
             self.dismiss(animated: false)
@@ -55,6 +68,8 @@ extension MarketingConsentViewController {
     }
     
     @objc private func disagreeButtonTapped() {
+        patchMarketingConsent(isAgreed: MarketingConsentRequestDTO(marketing: false))
+        
         rootView.dismissPopupView()
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.4){
             self.dismiss(animated: false)
