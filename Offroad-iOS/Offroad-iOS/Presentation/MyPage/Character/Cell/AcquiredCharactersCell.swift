ec07ef5e9c93e36e9757dcbd169367c6ba7ce122
codeJiwon <loopy0107@sookmyung.ac.kr>
2024-09-08 22:53:18 +0900

    [Feat/#163] notGainedCharacterCell에 shadowView와 자물쇠 이미지 적용

--- Offroad-iOS/Offroad-iOS/Presentation/MyPage/Character/Cell/AcquiredCharactersCell.swift
+++ Offroad-iOS/Offroad-iOS/Presentation/MyPage/Character/Cell/AcquiredCharactersCell.swift
@@ -29,6 +29,17 @@ class AcquiredCharactersCell: UICollectionViewCell {
         $0.font = UIFont.offroad(style: .iosTextContents)
     }
     
+    private let shadowView = UIView().then {
+        $0.backgroundColor = .blackOpacity(.black25)
+        $0.isHidden = true
+    }
+    
+    private let lockImageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFit
+        $0.image = UIImage(resource: .imgLock)
+        $0.isHidden = true
+    }
+    
     // MARK: - Life Cycle
     
     override init(frame: CGRect) {
@@ -45,7 +56,8 @@ class AcquiredCharactersCell: UICollectionViewCell {
     // MARK: - Setup Functions
     
     private func setupHierarchy() {
-        contentView.addSubviews(containerView, characterLabel)
+        contentView.addSubviews(containerView, characterLabel, shadowView)
+        shadowView.addSubview(lockImageView)
         containerView.addSubview(imageView)
     }
     
@@ -69,6 +81,17 @@ class AcquiredCharactersCell: UICollectionViewCell {
             make.centerX.equalToSuperview()
             make.bottom.equalToSuperview().inset(10)
         }
+        
+        shadowView.snp.makeConstraints { make in
+            make.edges.equalTo(contentView)
+        }
+        
+        lockImageView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().inset(75)
+            make.width.equalTo(33)
+            make.height.equalTo(37)
+        }
     }
     
     
@@ -79,6 +102,9 @@ class AcquiredCharactersCell: UICollectionViewCell {
         characterLabel.text = data.characterName
         contentView.backgroundColor = UIColor(hex: data.characterMainColorCode)
         containerView.backgroundColor = UIColor(hex: data.characterSubColorCode)
+        
+        shadowView.isHidden = true
+        lockImageView.isHidden = true
     }
     
     func notGainedCharacterCell(data: NotGainedCharacterList) {
@@ -86,5 +112,8 @@ class AcquiredCharactersCell: UICollectionViewCell {
         characterLabel.text = data.characterName
         contentView.backgroundColor = UIColor(hex: data.characterMainColorCode)
         containerView.backgroundColor = UIColor(hex: data.characterSubColorCode)
+        
+        shadowView.isHidden = false
+        lockImageView.isHidden = false
     }
 }
