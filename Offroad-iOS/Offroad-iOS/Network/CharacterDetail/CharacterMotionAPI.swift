9f975c3f7299f07df7a6214161a4fb2b68211971
codeJiwon <loopy0107@sookmyung.ac.kr>
2024-09-09 02:39:14 +0900

    [Feat/#169] 캐릭터 모션 목록 DTO, API, Service 파일 생성

---
+++ Offroad-iOS/Offroad-iOS/Network/CharacterDetail/CharacterMotionAPI.swift
@@ -0,0 +1,48 @@
+//
+//  CharacterMotionAPI.swift
+//  Offroad-iOS
+//
+//  Created by  정지원 on 9/9/24.
+//
+
+import Foundation
+
+import Moya
+
+enum CharacterMotionAPI {
+    case getCharacterMotionList(characterId: Int)
+}
+
+extension CharacterMotionAPI: BaseTargetType {
+    
+    var headerType: HeaderType { return .accessTokenHeaderForGet }
+    
+    var parameter: [String : Any]? {
+        switch self {
+        case .getCharacterMotionList:
+            return .none
+        }
+    }
+    var path: String {
+        switch self {
+        case .getCharacterMotionList(let characterId):
+            return "/characters/\(characterId)"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .getCharacterMotionList:
+            return .get
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .getCharacterMotionList:
+            return .requestPlain
+        }
+    }
+}
+
+
--- Offroad-iOS/Offroad-iOS/Presentation/MyPage/Character/ViewController/CharacterDetailViewController.swift
+++ Offroad-iOS/Offroad-iOS/Presentation/MyPage/Character/ViewController/CharacterDetailViewController.swift
@@ -15,7 +15,7 @@ final class CharacterDetailViewController: UIViewController {
     private let characterId: Int
     
     private let characterDetailView = CharacterDetailView()
-    private var characterDetailList: [GainedCharacterList]?
+    private var characterDetailList: [CharacterDetailList]?
     
     // MARK: - Life Cycle
     
@@ -68,30 +68,6 @@ final class CharacterDetailViewController: UIViewController {
             """
             characterDetailView.detailLabel.setLineSpacing(spacing: 5)
             
-        case "character_2":
-            view.backgroundColor = UIColor.primary(.characterSelectBg2)
-            characterDetailView.characterLogoImage.image = UIImage(resource: .character2Logo)
-            characterDetailView.characterImage.image = UIImage(named: imageName)
-            characterDetailView.nameLabel.text = "오푸"
-            characterDetailView.detailLabel.text = """
-            세상을 모험하는 건 정말 즐거운 일이야!
-            넘치는 열정을 가진 탐험가 오푸예요.
-            안경을 쓰면 또 다른 세상이 펼쳐져요!
-            """
-            characterDetailView.detailLabel.setLineSpacing(spacing: 5)
-            
-        case "character_3":
-            view.backgroundColor = UIColor.primary(.characterSelectBg1)
-            characterDetailView.characterLogoImage.image = UIImage(resource: .character3Logo)
-            characterDetailView.characterImage.image = UIImage(named: imageName)
-            characterDetailView.nameLabel.text = "루미"
-            characterDetailView.detailLabel.text = """
-            재밌는 걸 보면 두 눈이 반짝!
-            호기심이 많은 탐험가 루미예요.
-            파우치에는 최소한의 탐험 키트가 들어있답니다.
-            """
-            characterDetailView.detailLabel.setLineSpacing(spacing: 5)
-            
         default:
             view.backgroundColor = UIColor.gray
         }
@@ -100,8 +76,16 @@ final class CharacterDetailViewController: UIViewController {
     func getCharacterDetailInfo() {
         NetworkService.shared.characterDetailService.getAcquiredCharacterInfo(characterId: characterId) { response in
             switch response {
-            case .success(let data):
-                print("Character details: \(data)")
+            case .success(let characterDetailResponse):
+                guard let characterData = characterDetailResponse?.data else { return }
+                
+                print("Character details: \(characterData)")
+                self.view.backgroundColor = UIColor(hex: characterData.characterSubColorCode)
+                self.characterDetailView.characterImage.fetchSvgURLToImageView(svgUrlString: characterData.characterBaseImageUrl)
+                self.characterDetailView.characterLogoImage.fetchSvgURLToImageView(svgUrlString: characterData.characterIconImageUrl)
+                self.characterDetailView.nameLabel.text = characterData.characterName
+                self.characterDetailView.titleLabel.text = characterData.characterSummaryDescription
+                self.characterDetailView.detailLabel.text = characterData.characterDescription
                 
                 DispatchQueue.main.async {
                     self.characterDetailView.collectionView.reloadData()
@@ -124,15 +108,17 @@ extension CharacterDetailViewController: UICollectionViewDelegate, UICollectionV
     // MARK: - CollectionView Func
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 4
+        return 3
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CharacterDetailCell", for: indexPath) as! CharacterDetailCell
         let isNew = indexPath.item == 0
-        cell.configureCharacterImage(imageName: imageName, isNew: isNew)
-        
-        // 높이 업데이트를 호출
+//        let characterData = characterDetailList[indexPath.item]
+//        
+//        cell.contentView.backgroundColor = UIColor(hex: characterData.characterMainColorCode)
+//        cell.containerView.backgroundColor = UIColor(hex: characterData.characterSubColorCode)
+//        
         DispatchQueue.main.async {
             self.characterDetailView.updateCollectionViewHeight()
         }
