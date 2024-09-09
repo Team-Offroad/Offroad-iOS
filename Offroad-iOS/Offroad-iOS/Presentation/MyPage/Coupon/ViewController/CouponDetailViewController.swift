05d692f142eaf8430506c9e20896e055dccc6085
Minsung Kim <mskim4048@naver.com>
2024-09-10 03:33:34 +0900

    [Del/#172] 필요없는 코드 삭제

--- Offroad-iOS/Offroad-iOS/Presentation/MyPage/Coupon/ViewController/CouponDetailViewController.swift
+++ Offroad-iOS/Offroad-iOS/Presentation/MyPage/Coupon/ViewController/CouponDetailViewController.swift
@@ -53,42 +53,12 @@ class CouponDetailViewController: UIViewController {
     private func setupTarget() {
         couponDetailView.customBackButton.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
         couponDetailView.useButton.addTarget(self, action: #selector(didTapUseButton), for: .touchUpInside)
-        couponDetailView.couponUsagePopupView.couponCodeTextField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
-        couponDetailView.couponUsagePopupView.closeButton.addTarget(self, action: #selector(closeButtonTapped), for: .touchUpInside)
-        couponDetailView.couponUsagePopupView.okButton.addTarget(self, action: #selector(okButtonTapped), for: .touchUpInside)
-        
-        
-        setupCloseButton { [weak self] in
-            self?.dismissCouponUsagePopupView()
-        }
-    }
-    
-    private func presentCouponUsagePopupView() {
-        couponDetailView.couponUsagePopupView.isHidden = false
-        couponDetailView.couponUsagePopupView.layer.zPosition = 999
-        presentPopupView()
-    }
-    
-    private func dismissCouponUsagePopupView() {
-        dismissPopupView()
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
-            self.couponDetailView.couponUsagePopupView.isHidden = true
-        }
     }
     
     private func setupCloseButton(action: @escaping CouponCodeInputPopupView.CloseButtonAction) {
         couponDetailView.couponUsagePopupView.closeButtonAction = action
     }
     
-    private func presentPopupView() {
-        couponDetailView.couponUsagePopupView.backgroundColor = .blackOpacity(.black55)
-        couponDetailView.couponUsagePopupView.popupView.executePresentPopupAnimation()
-    }
-    
-    private func dismissPopupView() {
-        couponDetailView.couponUsagePopupView.backgroundColor = .clear
-        couponDetailView.couponUsagePopupView.popupView.executeDismissPopupAnimation()
-    }
 }
 
 extension CouponDetailViewController {
@@ -113,14 +83,5 @@ extension CouponDetailViewController {
         present(CouponCodeInputPopupViewController(), animated: false)
     }
     
-    @objc private func closeButtonTapped() {
-        couponDetailView.couponUsagePopupView.closeButtonAction?()
-        dismissPopupView()
-    }
-    
-    @objc private func okButtonTapped() {
-        couponDetailView.couponUsagePopupView.closeButtonAction?()
-        dismissPopupView()
-    }
 }
 
