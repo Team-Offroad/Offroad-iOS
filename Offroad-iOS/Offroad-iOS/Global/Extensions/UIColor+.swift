a99f8ce896e68b88fc42c2d80b498a51886dbb25
codeJiwon <loopy0107@sookmyung.ac.kr>
2024-09-10 20:03:49 +0900

    [Merge/#167] main을 167 브랜치로 머지

---
+++ Offroad-iOS/Offroad-iOS/Global/Extensions/UIColor+.swift
@@ -0,0 +1,36 @@
+//
+//  UIColor+.swift
+//  Offroad-iOS
+//
+//  Created by  정지원 on 9/8/24.
+//
+
+import UIKit
+
+/// 8자리 또는 6자리 Hex String을 UIColor로 변환
+/// > 사용 예시 : `UIColor(hex: data.characterMainColorCode)`
+extension UIColor {
+    convenience init?(hex: String) {
+        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
+        
+        var rgb: UInt64 = 0
+        var length = 0
+        
+        // Hex 코드를 UInt64로 변환
+        Scanner(string: hexSanitized).scanHexInt64(&rgb)
+        length = hexSanitized.count
+
+        // 8자리일 경우 앞의 두 자리를 Alpha로 처리
+        if length == 8 {
+            let r = CGFloat((rgb & 0x00FF0000) >> 16) / 255.0
+            let g = CGFloat((rgb & 0x0000FF00) >> 8) / 255.0
+            let b = CGFloat(rgb & 0x000000FF) / 255.0
+            let a = CGFloat((rgb & 0xFF000000) >> 24) / 255.0
+
+            self.init(red: r, green: g, blue: b, alpha: a)
+        } else {
+            return nil
+        }
+    }
+}
---
+++ Offroad-iOS/Offroad-iOS/Network/CharacterList/CharacterListService.swift
@@ -0,0 +1,35 @@
+//
+//  AcquiredCharacterService.swift
+//  Offroad-iOS
+//
+//  Created by  정지원 on 9/8/24.
+//
+
+import Foundation
+
+import Moya
+
+protocol CharacterListServiceProtocol {
+    func getCharacterListInfo(completion: @escaping (NetworkResult<CharacterListResponseDTO>) -> ())
+}
+
+final class CharacterListService: BaseService, CharacterListServiceProtocol {
+    let provider = MoyaProvider<CharacterListAPI>.init(session: Session(interceptor: TokenInterceptor.shared), plugins: [MoyaPlugin()])
+
+    func getCharacterListInfo(completion: @escaping (NetworkResult<CharacterListResponseDTO>) -> ()) {
+        provider.request(.getCharacterListInfo) { result in
+            switch result {
+            case .success(let response):
+                let networkResult: NetworkResult<CharacterListResponseDTO> = self.fetchNetworkResult(
+                    statusCode: response.statusCode,
+                    data: response.data
+                )
+                completion(networkResult)
+            case .failure(let err):
+                print(err)
+            }
+        }
+    }
+}
+
+
