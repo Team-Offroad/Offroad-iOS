fdc20d5313301de5fda1d8714c60c402a6693873
Minsung Kim <mskim4048@naver.com>
2024-09-12 15:31:02 +0900

    [Fix/#174] OFRPopupButton 파일 및 타입 추가

---
+++ Offroad-iOS/Offroad-iOS/Global/Components/PopupController/OFRPopupButton.swift
@@ -0,0 +1,20 @@
+//
+//  OFRPopupButton.swift
+//  Offroad-iOS
+//
+//  Created by 김민성 on 9/12/24.
+//
+
+import UIKit
+
+class OFRPopupButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
