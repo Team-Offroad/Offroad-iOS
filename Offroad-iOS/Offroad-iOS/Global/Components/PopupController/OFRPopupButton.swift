8fe33b2d7bc08c1834459255c0a3e40fe3e32727
Minsung Kim <mskim4048@naver.com>
2024-09-12 15:36:04 +0900

    [Chore/#174] OFRPopupButton 기본 틀 작성

--- Offroad-iOS/Offroad-iOS/Global/Components/PopupController/OFRPopupButton.swift
+++ Offroad-iOS/Offroad-iOS/Global/Components/PopupController/OFRPopupButton.swift
@@ -7,14 +7,24 @@
 
 import UIKit
 
+
+
 class OFRPopupButton: UIButton {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    //MARK: - Properties
+    
+    //MARK: - UI Properties
+    
+    //MARK: - Life Cycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
