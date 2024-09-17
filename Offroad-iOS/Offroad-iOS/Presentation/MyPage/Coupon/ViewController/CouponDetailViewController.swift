3ef0ebd0cbef91674d03b4c553c6aca758fae2c0
codeJiwon <loopy0107@sookmyung.ac.kr>
2024-09-17 19:21:31 +0900

    Revert "[Merge/#169] main을 169 브랜치로 머지"
    This reverts commit 36b20f037658925dec9639d81f3901c1f16cac68.

--- Offroad-iOS/Offroad-iOS/Presentation/MyPage/Coupon/ViewController/CouponDetailViewController.swift
+++ Offroad-iOS/Offroad-iOS/Presentation/MyPage/Coupon/ViewController/CouponDetailViewController.swift
@@ -41,7 +41,7 @@ class CouponDetailViewController: UIViewController {
         couponDetailView.couponImageView.image = image
         couponDetailView.couponTitleLabel.text = title
         couponDetailView.couponDescriptionLabel.text = description
-        couponDetailView.couponDescriptionLabel.setLineSpacing(spacing: 5)
+        couponDetailView.couponDescriptionLabel.setLineHeight(percentage: 150)
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -53,42 +53,12 @@ class CouponDetailViewController: UIViewController {
     private func setupTarget() {
         couponDetailView.customBackButton.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
         couponDetailView.useButton.addTarget(self, action: #selector(didTapUseButton), for: .touchUpInside)
-        couponDetailView.couponUsagePopupView.codeTextField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
-        couponDetailView.couponUsagePopupView.closeButton.addTarget(self, action: #selector(closeButtonTapped), for: .touchUpInside)
-        couponDetailView.couponUsagePopupView.checkButton.addTarget(self, action: #selector(checkButtonTapped), for: .touchUpInside)
-        
-        
-        setupCloseButton { [weak self] in
-            self?.dismissCouponUsagePopupView()
-        }
     }
     
-    private func presentCouponUsagePopupView() {
-        couponDetailView.couponUsagePopupView.isHidden = false
-        couponDetailView.couponUsagePopupView.layer.zPosition = 999
-        presentPopupView()
-    }
-    
-    private func dismissCouponUsagePopupView() {
-        dismissPopupView()
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
-            self.couponDetailView.couponUsagePopupView.isHidden = true
-        }
-    }
-    
-    private func setupCloseButton(action: @escaping CouponUsagePopupView.CloseButtonAction) {
+    private func setupCloseButton(action: @escaping CouponCodeInputPopupView.CloseButtonAction) {
         couponDetailView.couponUsagePopupView.closeButtonAction = action
     }
     
-    private func presentPopupView() {
-        couponDetailView.couponUsagePopupView.backgroundColor = .blackOpacity(.black55)
-        couponDetailView.couponUsagePopupView.popupView.executePresentPopupAnimation()
-    }
-    
-    private func dismissPopupView() {
-        couponDetailView.couponUsagePopupView.backgroundColor = .clear
-        couponDetailView.couponUsagePopupView.popupView.executeDismissPopupAnimation()
-    }
 }
 
 extension CouponDetailViewController {
@@ -100,26 +70,18 @@ extension CouponDetailViewController {
     }
     
     @objc private func textFieldDidChange() {
-        let isTextFieldEmpty = couponDetailView.couponUsagePopupView.codeTextField.text?.isEmpty ?? true
+        let isTextFieldEmpty = couponDetailView.couponUsagePopupView.couponCodeTextField.text?.isEmpty ?? true
         
         if isTextFieldEmpty {
-            couponDetailView.couponUsagePopupView.checkButton.changeState(forState: .isDisabled)
+            couponDetailView.couponUsagePopupView.okButton.changeState(forState: .isDisabled)
         } else {
-            couponDetailView.couponUsagePopupView.checkButton.changeState(forState: .isEnabled)            }
+            couponDetailView.couponUsagePopupView.okButton.changeState(forState: .isEnabled)            }
     }
     
     @objc private func didTapUseButton() {
-        presentCouponUsagePopupView()
+        let popupViewController = CouponCodeInputPopupViewController()
+        present(popupViewController, animated: false)
     }
     
-    @objc private func closeButtonTapped() {
-        couponDetailView.couponUsagePopupView.closeButtonAction?()
-        dismissPopupView()
-    }
-    
-    @objc private func checkButtonTapped() {
-        couponDetailView.couponUsagePopupView.closeButtonAction?()
-        dismissPopupView()
-    }
 }
 
