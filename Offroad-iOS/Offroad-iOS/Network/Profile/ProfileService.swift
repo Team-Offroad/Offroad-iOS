4d1482fd1e70c6482723026253396ded0e4716f0
codeJiwon <loopy0107@sookmyung.ac.kr>
2024-09-22 03:59:10 +0900

    [Chore/#192] 함수명 변경

--- Offroad-iOS/Offroad-iOS/Network/Profile/ProfileService.swift
+++ Offroad-iOS/Offroad-iOS/Network/Profile/ProfileService.swift
@@ -10,7 +10,7 @@ import Foundation
 import Moya
 
 protocol ProfileServiceProtocol {
-    func updateProfile(body: ProfileUpdateRequestDTO, completion: @escaping (NetworkResult<ProfileUpdateResponseDTO>) -> ())
+    func patchUpdateProfile(body: ProfileUpdateRequestDTO, completion: @escaping (NetworkResult<ProfileUpdateResponseDTO>) -> ())
     func postDeleteAccount(body: DeleteAccountRequestDTO, completion: @escaping (NetworkResult<DeleteAccountResponseDTO>) -> ())
     func patchMarketingConsent(body: MarketingConsentRequestDTO, completion: @escaping (NetworkResult<MarketingConsentResponseDTO>) -> ())
     func getUserInfo(completion: @escaping (NetworkResult<UserInfoResponseDTO>) -> ())
@@ -19,8 +19,8 @@ protocol ProfileServiceProtocol {
 final class ProfileService: BaseService, ProfileServiceProtocol {
     let provider = MoyaProvider<ProfileAPI>.init(session: Session(interceptor: TokenInterceptor.shared), plugins: [MoyaPlugin()])
     
-    func updateProfile(body: ProfileUpdateRequestDTO, completion: @escaping (NetworkResult<ProfileUpdateResponseDTO>) -> ()) {
-        provider.request(.updateProfile(body: body)) { result in
+    func patchUpdateProfile(body: ProfileUpdateRequestDTO, completion: @escaping (NetworkResult<ProfileUpdateResponseDTO>) -> ()) {
+        provider.request(.patchUpdateProfile(body: body)) { result in
             switch result {
             case .success(let response):
                 let networkResult: NetworkResult<ProfileUpdateResponseDTO> = self.fetchNetworkResult(
