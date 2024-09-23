132603624eee44bd12e7b8ac90131c4220c1d372
JoHyerin <gpfls468@gmail.com>
2024-09-23 16:16:54 +0900

    [Chore/#191] prepareForReuse() 추가

--- Offroad-iOS/Offroad-iOS/Presentation/MyPage/Settings/Component/SettingBase/Cell/SettingBaseCollectionViewCell.swift
+++ Offroad-iOS/Offroad-iOS/Presentation/MyPage/Settings/Component/SettingBase/Cell/SettingBaseCollectionViewCell.swift
@@ -42,6 +42,12 @@ final class SettingBaseCollectionViewCell: UICollectionViewCell {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+
+        listLabel.text = nil
+    }
 }
 
 extension SettingBaseCollectionViewCell {
