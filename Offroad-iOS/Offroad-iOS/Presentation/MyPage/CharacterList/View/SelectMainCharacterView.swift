d64b50d3e7bfd44a1fa514719f9fabaaf5752bef
codeJiwon <loopy0107@sookmyung.ac.kr>
2024-09-24 05:44:22 +0900

    [Feat/#202] 대표 캐릭터 토스트메세지 UI 구현

--- Offroad-iOS/Offroad-iOS/Presentation/MyPage/CharacterList/View/SelectMainCharacterView.swift
+++ Offroad-iOS/Offroad-iOS/Presentation/MyPage/CharacterList/View/SelectMainCharacterView.swift
@@ -4,5 +4,70 @@
 //
 //  Created by  정지원 on 9/24/24.
 //
+import UIKit
 
-import Foundation
+import SnapKit
+import Then
+
+class SelectMainCharacterView: UIView {
+
+    // MARK: - UI Properties
+    
+    private let selectMessageBackgroundView = UIView().then {
+        $0.backgroundColor = UIColor.blackOpacity(.black55)
+        $0.layer.cornerRadius = 10
+    }
+    
+    private let checkmarkImageView = UIImageView().then {
+        $0.image = UIImage(resource: .btnChecked)
+    }
+    
+    private let selectMessageLabel = UILabel().then {
+        $0.font = UIFont.offroad(style: .iosTextMedium)
+        $0.textColor = UIColor.primary(.white)
+    }
+    
+    // MARK: - Life Cycle
+    
+    init(frame: CGRect, characterName: String) {
+        super.init(frame: frame)
+        
+        setupHierarchy()
+        setupLayout()
+        setMessage(characterName: characterName)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Private Func
+    
+    private func setupHierarchy() {
+        addSubview(selectMessageBackgroundView)
+        selectMessageBackgroundView.addSubview(checkmarkImageView)
+        selectMessageBackgroundView.addSubview(selectMessageLabel)
+    }
+    
+    private func setupLayout() {
+        selectMessageBackgroundView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        checkmarkImageView.snp.makeConstraints { make in
+            make.leading.equalTo(selectMessageBackgroundView).inset(20)
+            make.centerY.equalTo(selectMessageBackgroundView)
+            make.size.equalTo(24)
+        }
+        
+        selectMessageLabel.snp.makeConstraints { make in
+            make.leading.equalTo(checkmarkImageView.snp.trailing).offset(10)
+            make.centerY.equalTo(selectMessageBackgroundView)
+        }
+    }
+    
+    private func setMessage(characterName: String) {
+        selectMessageLabel.text = "‘\(characterName)’로 대표 캐릭터가 변경되었어요!"
+        selectMessageLabel.highlightText(targetText: characterName, font: UIFont.offroad(style: .iosTextBold))
+    }
+}
