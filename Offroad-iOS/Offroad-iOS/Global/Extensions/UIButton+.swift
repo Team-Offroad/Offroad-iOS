386622dd2d093f2e019af0ecf8469b216fe25133
Minsung Kim <mskim4048@naver.com>
2024-10-03 21:58:25 +0900

    [Add/#208] Configuration을 이용하여 UIButton의 배경색과 title의 font를 변경하는 확장 함수 구현

--- Offroad-iOS/Offroad-iOS/Global/Extensions/UIButton+.swift
+++ Offroad-iOS/Offroad-iOS/Global/Extensions/UIButton+.swift
@@ -24,5 +24,94 @@ extension UIButton {
 
         setBackgroundImage(backgroundImage, for: state)
     }
+    
+    /// UIButton의 state에 따른 배경색을 설정하는 메서드
+    /// - Parameters:
+    ///   - normal: normal일 때의 font
+    ///   - highlighted: highlighted 됐을 때의 배경색
+    ///   - focused: focused 됐을 떄의 배경색
+    ///   - selected: selected 됐을 때의 배경색
+    ///   - disabled: disabled 됐을 떄의 배경색
+    /// - Description
+    /// normal을 제외한 나머지 매개변수은 매개변수의 값이 nil일 경우 normal의 값이 할당됨.
+    func configureBackgroundColorWhen(
+        normal: UIColor,
+        highlighted: UIColor? = nil,
+        focused: UIColor? = nil,
+        selected: UIColor? = nil,
+        disabled: UIColor? = nil
+    ) {
+        var newConfiguration: UIButton.Configuration
+        if let configuration {
+            newConfiguration = configuration
+        } else {
+            newConfiguration = UIButton.Configuration.filled()
+        }
+        
+        let transformer = UIConfigurationColorTransformer { incomingColor in
+            switch self.state {
+            case .normal:
+                return normal
+            case .highlighted:
+                return highlighted ?? normal
+            case .focused:
+                return focused ?? normal
+            case .selected:
+                return selected ?? normal
+            case .disabled:
+                return disabled ?? .gray
+            default:
+                return self.backgroundColor!
+            }
+        }
+        newConfiguration.background.backgroundColorTransformer = transformer
+        self.configuration = newConfiguration
+    }
+    
+    /// UIButton의 state에 따른 title의 font를 설정하는 메서드.
+    /// - Parameters:
+    ///   - normal: normal일 때의 폰트.
+    ///   - highlighted: highlight 됐을 때의 폰트.
+    ///   - focused: focused 됐을 때의 폰트.
+    ///   - selected: selected 됐을 때의 폰트.
+    ///   - disabled: disabled 됐을 때의 폰트.
+    /// - Description
+    /// normal을 제외한 나머지 매개변수은 매개변수의 값이 nil일 경우 normal의 값이 할당됨.
+    func configureTitleFontWhen(
+        normal: UIFont,
+        highlighted: UIFont? = nil,
+        focused: UIFont? = nil,
+        selected: UIFont? = nil,
+        disabled: UIFont? = nil
+    ) {
+        var newConfiguration: UIButton.Configuration
+        if let configuration {
+            newConfiguration = configuration
+        } else {
+            newConfiguration = UIButton.Configuration.filled()
+        }
+        
+        let transformer = UIConfigurationTextAttributesTransformer { incoming in
+            var outgoing = incoming
+            switch self.state {
+            case .normal:
+                outgoing.font = normal
+            case .highlighted:
+                outgoing.font = highlighted ?? normal
+            case .focused:
+                outgoing.font = focused ?? normal
+            case .selected:
+                outgoing.font = selected ?? normal
+            case .disabled:
+                outgoing.font = disabled ?? normal
+            default:
+                break
+            }
+            return outgoing
+        }
+        newConfiguration.titleTextAttributesTransformer = transformer
+        self.configuration = newConfiguration
+    }
+    
 }
 
