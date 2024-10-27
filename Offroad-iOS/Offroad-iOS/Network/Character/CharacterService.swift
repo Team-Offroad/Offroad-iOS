1d30452f7ceb03b37fb73c09d7f9fd3a00f627d5
Minsung Kim <mskim4048@naver.com>
2024-10-27 14:52:05 +0900

    [Fix/#225] 브랜치 머지 후 네트워크 통신 실패 시 .networkFail 에러를 제대로 전달하지 않는 문제 해결

--- Offroad-iOS/Offroad-iOS/Network/Character/CharacterService.swift
+++ Offroad-iOS/Offroad-iOS/Network/Character/CharacterService.swift
@@ -68,8 +68,17 @@ final class CharacterService: BaseService, CharacterServiceProtocol {
                     data: response.data
                 )
                 completion(networkResult)
-            case .failure(let err):
-                print(err)
+            case .failure(let error):
+                print(error.localizedDescription)
+                switch error {
+                case .underlying(let error, let response):
+                    print(error.localizedDescription)
+                    if response == nil {
+                        completion(.networkFail)
+                    }
+                default:
+                    print(error.localizedDescription)
+                }
             }
         }
     }
@@ -84,8 +93,17 @@ final class CharacterService: BaseService, CharacterServiceProtocol {
                     data: response.data
                 )
                 completion(networkResult)
-            case .failure(let err):
-                print(err)
+            case .failure(let error):
+                print(error.localizedDescription)
+                switch error {
+                case .underlying(let error, let response):
+                    print(error.localizedDescription)
+                    if response == nil {
+                        completion(.networkFail)
+                    }
+                default:
+                    print(error.localizedDescription)
+                }
             }
         }
     }
@@ -100,8 +118,17 @@ final class CharacterService: BaseService, CharacterServiceProtocol {
                     data: response.data
                 )
                 completion(networkResult)
-            case .failure(let err):
-                print(err)
+            case .failure(let error):
+                print(error.localizedDescription)
+                switch error {
+                case .underlying(let error, let response):
+                    print(error.localizedDescription)
+                    if response == nil {
+                        completion(.networkFail)
+                    }
+                default:
+                    print(error.localizedDescription)
+                }
             }
         }
     }
