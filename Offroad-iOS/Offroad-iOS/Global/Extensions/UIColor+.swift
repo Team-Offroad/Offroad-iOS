d363ec53cbd45eff6ab3ee70b0abb839e9f1c0d8
Minsung Kim <mskim4048@naver.com>
2024-11-11 00:16:28 +0900

    [Chore/#257] UIColor 확장 코드 잘못된 부분 개선

--- Offroad-iOS/Offroad-iOS/Global/Extensions/UIColor+.swift
+++ Offroad-iOS/Offroad-iOS/Global/Extensions/UIColor+.swift
@@ -23,12 +23,18 @@ extension UIColor {
 
         // 8자리일 경우 앞의 두 자리를 Alpha로 처리
         if length == 8 {
-            let r = CGFloat((rgb & 0x00FF0000) >> 16) / 255.0
-            let g = CGFloat((rgb & 0x0000FF00) >> 8) / 255.0
-            let b = CGFloat(rgb & 0x000000FF) / 255.0
-            let a = CGFloat((rgb & 0xFF000000) >> 24) / 255.0
+            let r = CGFloat((rgb & 0xFF000000) >> 16) / 255.0
+            let g = CGFloat((rgb & 0x00FF0000) >> 8) / 255.0
+            let b = CGFloat(rgb & 0x0000FF00) / 255.0
+            let a = CGFloat((rgb & 0x000000FF) >> 24) / 255.0
 
             self.init(red: r, green: g, blue: b, alpha: a)
+        } else if length == 6 {
+            let r = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
+            let g = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
+            let b = CGFloat(rgb & 0x0000FF) / 255.0
+            
+            self.init(red: r, green: g, blue: b, alpha: 1)
         } else {
             return nil
         }
