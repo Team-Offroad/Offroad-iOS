b8428e14a9399bcefc0a8d4edf47733bf9c4e4e4
Minsung Kim <mskim4048@naver.com>
2024-11-14 15:40:25 +0900

    [Feat/#276] CharacterChatService 구현

--- Offroad-iOS/Offroad-iOS/Network/CharacterChat/CharacterChatService.swift
+++ Offroad-iOS/Offroad-iOS/Network/CharacterChat/CharacterChatService.swift
@@ -6,3 +6,65 @@
 //
 
 import Foundation
+
+import Moya
+
+protocol CharacterChatServiceProtocol {
+    func postChat(body: CharacterChatPostRequestDTO, completion: @escaping (NetworkResult<CharacterChatPostResponseDTO>) -> Void)
+    func getChatLog(completion: @escaping (NetworkResult<CharacterChatGetResponseDTO>) -> Void)
+}
+
+final class CharacterChatService: BaseService, CharacterChatServiceProtocol {
+    
+    let provider = MoyaProvider<CharacterChatAPI>.init(session: Session(interceptor: TokenInterceptor.shared), plugins: [MoyaPlugin()])
+    
+    func postChat(body: CharacterChatPostRequestDTO, completion: @escaping (NetworkResult<CharacterChatPostResponseDTO>) -> Void) {
+        provider.request(.postChat(body: body)) { result in
+            switch result {
+            case .success(let response):
+                let networkResult: NetworkResult<CharacterChatPostResponseDTO> = self.fetchNetworkResult(
+                    statusCode: response.statusCode,
+                    data: response.data
+                )
+                completion(networkResult)
+            case .failure(let error):
+                print(error.localizedDescription)
+                switch error {
+                case .underlying(let erorr, let response):
+                    print(error.localizedDescription)
+                    if response == nil {
+                        completion(.networkFail)
+                    }
+                default:
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
+    
+    func getChatLog(completion: @escaping (NetworkResult<CharacterChatGetResponseDTO>) -> Void) {
+        provider.request(.getChatLog) { result in
+            switch result {
+            case .success(let response):
+                let networkResult: NetworkResult<CharacterChatGetResponseDTO> = self.fetchNetworkResult(
+                    statusCode: response.statusCode,
+                    data: response.data
+                )
+                completion(networkResult)
+            case .failure(let error):
+                print(error.localizedDescription)
+                switch error {
+                case .underlying(let erorr, let response):
+                    print(error.localizedDescription)
+                    if response == nil {
+                        completion(.networkFail)
+                    }
+                default:
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
+    
+    
+}
