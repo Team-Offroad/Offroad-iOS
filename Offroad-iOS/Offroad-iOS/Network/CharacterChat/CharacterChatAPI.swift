741326dafa8a27e664ea63626ab41dcfb754d37e
Minsung Kim <mskim4048@naver.com>
2024-11-14 11:00:46 +0900

    [Feat/#276] CharacterChatAPI 구현

--- Offroad-iOS/Offroad-iOS/Network/CharacterChat/CharacterChatAPI.swift
+++ Offroad-iOS/Offroad-iOS/Network/CharacterChat/CharacterChatAPI.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+import Moya
+
+enum CharacterChatAPI {
+    case postChat(body: CharacterChatPostRequestDTO)
+    case getChatLog
+}
+
+extension CharacterChatAPI: BaseTargetType {
+    var headerType: HeaderType {
+        .accessTokenHeaderForGet
+    }
+    
+    var path: String {
+        "/chats"
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .postChat: .post
+        case .getChatLog: .get
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .postChat(body: let body):
+            return .requestJSONEncodable(body)
+        case .getChatLog:
+            return .requestPlain
+        }
+    }
+    
+    
+}
