63f2900ffe86fea8a33fa45d14c37c1c0a8f04e2
JoHyerin <gpfls468@gmail.com>
2024-11-16 02:40:38 +0900

    [Add/#266] 푸시 알림을 위한 AppDelegate, NotificationServiceExtension 설정(임시)

--- Offroad-iOS/Offroad-iOS/Application/AppDelegate.swift
+++ Offroad-iOS/Offroad-iOS/Application/AppDelegate.swift
@@ -7,7 +7,10 @@
 
 import UIKit
 
+import Firebase
+import FirebaseMessaging
 import KakaoSDKCommon
+import UserNotifications
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -20,6 +23,30 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         if let nativeAppKey = Bundle.main.infoDictionary?["NATIVE_APP_KEY"] as? String {
             KakaoSDK.initSDK(appKey: nativeAppKey)
         }
+        
+        // 파이어베이스 설정
+        FirebaseApp.configure()
+        
+        // 앱 실행 시 사용자에게 알림 허용 권한을 받음
+        UNUserNotificationCenter.current().delegate = self
+        
+        let category = UNNotificationCategory(identifier: "category",
+                                              actions: [],
+                                              intentIdentifiers: [],
+                                              options: .customDismissAction)
+        UNUserNotificationCenter.current().setNotificationCategories([category])
+        
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound] // 필요한 알림 권한을 설정
+        UNUserNotificationCenter.current().requestAuthorization(
+            options: authOptions,
+            completionHandler: { _, _ in }
+        )
+        
+        // UNUserNotificationCenterDelegate를 구현한 메서드를 실행시킴
+        application.registerForRemoteNotifications()
+        
+        // 파이어베이스 Meesaging 설정
+        Messaging.messaging().delegate = self
         
         return true
     }
@@ -41,3 +68,40 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 }
 
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    
+    // 백그라운드에서 푸시 알림을 탭했을 때 실행
+    func application(_ application: UIApplication,
+                     didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        print("APNS token: \(deviceToken)")
+        Messaging.messaging().apnsToken = deviceToken
+    }
+    
+    // Foreground(앱 켜진 상태)에서도 알림 오는 설정
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.list, .banner])
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        let userInfo = response.notification.request.content.userInfo
+        
+        print(userInfo)
+        
+        completionHandler()
+    }
+}
+
+extension AppDelegate: MessagingDelegate {
+    
+    // 파이어베이스 MessagingDelegate 설정
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+      print("Firebase registration token: \(String(describing: fcmToken))")
+
+      let dataDict: [String: String] = ["token": fcmToken ?? ""]
+      NotificationCenter.default.post(
+        name: Notification.Name("FCMToken"),
+        object: nil,
+        userInfo: dataDict
+      )
+    }
+}
