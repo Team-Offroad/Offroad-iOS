310e022edb52771db18ef123f1b590bfdcc1535e
JoHyerin <gpfls468@gmail.com>
2024-11-23 04:09:37 +0900

    [Chore/#266] 채팅 메시지 띄우는 코드 추가

--- Offroad-iOS/Offroad-iOS/Presentation/Home/ViewController/HomeViewController.swift
+++ Offroad-iOS/Offroad-iOS/Presentation/Home/ViewController/HomeViewController.swift
@@ -188,6 +188,7 @@ extension HomeViewController {
                 redirectNoticePost()
             case "CHARACTER_CHAT":
                 ORBCharacterChatManager.shared.startChat()
+                ORBCharacterChatManager.shared.showCharacterChatBox(character: self.pushType?.data?["characterName"] as! String, message: self.pushType?.data?["message"] as! String, mode: .withoutReplyButtonExpanded)
             default:
                 break
             }
