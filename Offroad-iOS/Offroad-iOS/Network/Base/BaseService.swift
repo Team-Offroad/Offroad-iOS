d41dd5088ce490e7599eed2d499ea140010ec135
Minsung Kim <mskim4048@naver.com>
2024-12-02 10:38:44 +0900

    [Fix/#365] 기본 서버 통신 코드에 ErrorResponseDTO 사용할 수 있도록 함수 변경

--- Offroad-iOS/Offroad-iOS/Network/Base/BaseService.swift
+++ Offroad-iOS/Offroad-iOS/Network/Base/BaseService.swift
@@ -11,41 +11,56 @@ class BaseService {
     
     /// 200 받았을 때 decoding 할 데이터가 있는 경우 (대부분의 GET)
     func fetchNetworkResult<T: Decodable>(statusCode: Int, data: Data) -> NetworkResult<T> {
+        var decodeErrorResponse: ErrorResponseDTO? {
+            fetchDecodeData(data: data, responseType: ErrorResponseDTO.self)
+        }
+        
         switch statusCode {
         case 200, 201, 202:
             if let decodedData = fetchDecodeData(data: data, responseType: T.self) {
                 return .success(decodedData)
-            } else { return .decodeErr }
+            } else { return .decodeErr() }
         case 204: return .success(nil)
-        case 400: return .requestErr
-        case 401: return .unAuthentication
-        case 403: return .unAuthorization
-        case 404: return .apiArr
-        case 405: return .pathErr
-        case 409: return .requestErr
-        case 500: return .serverErr
-        default: return .networkFail
+        case 400: return .requestErr(decodeErrorResponse)
+        case 401: return .unAuthentication(decodeErrorResponse)
+        case 403: return .unAuthorization(decodeErrorResponse)
+        case 404: return .apiArr(decodeErrorResponse)
+        case 405: return .pathErr(decodeErrorResponse)
+        case 409: return .requestErr(decodeErrorResponse)
+        case 500...599: return .serverErr(decodeErrorResponse)
+        default: return .networkFail(decodeErrorResponse)
         }
     }
     
     /// 200 받았을 때 decoding 할 데이터가 없는 경우 (대부분의 PATCH, PUT, DELETE)
     func fetchNetworkResult(statusCode: Int, data: Data) -> NetworkResult<Any> {
+        var decodeErrorResponse: ErrorResponseDTO? {
+            fetchDecodeData(data: data, responseType: ErrorResponseDTO.self)
+        }
+        
         switch statusCode {
         case 200, 201, 204: return .success(nil)
-        case 400: return .requestErr
-        case 401: return .unAuthentication
-        case 403: return .unAuthorization
-        case 404: return .apiArr
-        case 405: return .pathErr
-        case 409: return .requestErr
-        case 500: return .serverErr
-        default: return .networkFail
+        case 400: return .requestErr(decodeErrorResponse)
+        case 401: return .unAuthentication(decodeErrorResponse)
+        case 403: return .unAuthorization(decodeErrorResponse)
+        case 404: return .apiArr(decodeErrorResponse)
+        case 405: return .pathErr(decodeErrorResponse)
+        case 409: return .requestErr(decodeErrorResponse)
+        case 500...599: return .serverErr(decodeErrorResponse)
+        default: return .networkFail()
         }
     }
     
     func fetchDecodeData<T: Decodable>(data: Data, responseType: T.Type) -> T? {
         let decoder = JSONDecoder()
         if let decodedData = try? decoder.decode(responseType, from: data){
+            return decodedData
+        } else { return nil }
+    }
+    
+    func fetchErrorMessageData(data: Data) -> ErrorResponseDTO? {
+        let decoder = JSONDecoder()
+        if let decodedData = try? decoder.decode(ErrorResponseDTO.self, from: data) {
             return decodedData
         } else { return nil }
     }
