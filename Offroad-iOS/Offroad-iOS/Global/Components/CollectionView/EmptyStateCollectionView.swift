2f96f54f24d3b104d4f36e53285f0f48c7ec88cb
Minsung Kim <mskim4048@naver.com>
2024-12-23 03:22:40 +0900

    [Add/#356] EmptyStateCollectionView.swift 파일 추가 EmptyStateCollectionView는 엠티 케이스를 표현할 수 있는 UICollectionView를 상속한 타입..

---
+++ Offroad-iOS/Offroad-iOS/Global/Components/CollectionView/EmptyStateCollectionView.swift
@@ -0,0 +1,20 @@
+//
+//  EmptyStateCollectionView.swift
+//  Offroad-iOS
+//
+//  Created by 김민성 on 12/23/24.
+//
+
+import UIKit
+
+class EmptyStateCollectionView: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
