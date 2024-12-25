abca1947a864ac4d69ddda67ae484083a4fe1d31
Minsung Kim <mskim4048@naver.com>
2024-12-26 01:26:46 +0900

    [Add/#380] ORBMapViewController.swift 파일 추가

---
+++ Offroad-iOS/Offroad-iOS/Presentation/QuestMap/ViewControllers/ORBMapViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ORBMapViewController.swift
+//  Offroad-iOS
+//
+//  Created by 김민성 on 12/26/24.
+//
+
+import UIKit
+
+class ORBMapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
