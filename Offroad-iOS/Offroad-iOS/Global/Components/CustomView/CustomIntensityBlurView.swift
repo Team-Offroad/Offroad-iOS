4d16db3cc49d3ac336e7cfb5bed57a0ff81f37fc
Minsung Kim <mskim4048@naver.com>
2024-12-31 23:34:00 +0900

    [Add/#317] CustomIntensityBlurView 추가

---
+++ Offroad-iOS/Offroad-iOS/Global/Components/CustomView/CustomIntensityBlurView.swift
@@ -0,0 +1,20 @@
+//
+//  CustomIntensityBlurView.swift
+//  Offroad-iOS
+//
+//  Created by 김민성 on 12/31/24.
+//
+
+import UIKit
+
+class CustomIntensityBlurView: UIVisualEffectView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
