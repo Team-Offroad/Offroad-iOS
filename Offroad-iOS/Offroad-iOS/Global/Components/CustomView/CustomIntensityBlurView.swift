b7002542e77f8e7f1388674b8713a716702c65ad
Minsung Kim <mskim4048@naver.com>
2025-01-01 00:32:51 +0900

    [Feat/#317] CustomIntensityBlurView 구현 블러의 세기를 원하는 값으로 설정할 수 있도록 UIVisualEffectView를 상속한 타입을 구현

--- Offroad-iOS/Offroad-iOS/Global/Components/CustomView/CustomIntensityBlurView.swift
+++ Offroad-iOS/Offroad-iOS/Global/Components/CustomView/CustomIntensityBlurView.swift
@@ -8,13 +8,29 @@
 import UIKit
 
 class CustomIntensityBlurView: UIVisualEffectView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    //MARK: - Properties
+    
+    private var animator = UIViewPropertyAnimator(duration: 1, curve: .linear)
+    
+    //MARK: - Life Cycle
+    
+    /// 원하는 세기의 블러를 적용한 UIVisualEffectView 생성
+    /// - Parameters:
+    ///   - style: blur의 style. UIBlurEffect.Style 타입
+    ///   - intensity: 적용할 블러의 세기 0에서 1 사이의 값을 가지며 0은 블러 전혀 없음, 1은 UIKit의 기본 블러 세기를 의미
+    init(blurStyle style: UIBlurEffect.Style, intensity: CGFloat) {
+        super.init(effect: nil)
+        animator.addAnimations { [weak self] in
+            guard let self else { return }
+            self.effect = UIBlurEffect(style: style)
+        }
+        animator.fractionComplete = intensity
+        animator.pausesOnCompletion = true
     }
-    */
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError()
+    }
 
 }
