00c1269da6d4ed07412a4b77391556ffa5feb2ec
Minsung Kim <mskim4048@naver.com>
2025-01-05 17:12:59 +0900

    [Chore/#423] TouchFeedback 타입 이름 변경 TouchFeedback -> ORBTouchFeedback

--- Offroad-iOS/Offroad-iOS/Global/Protocols/ORBTouchFeedback.swift
+++ Offroad-iOS/Offroad-iOS/Global/Protocols/ORBTouchFeedback.swift
@@ -1,5 +1,5 @@
 //
-//  TouchFeedback.swift
+//  ORBTouchFeedback.swift
 //  Offroad-iOS
 //
 //  Created by 김민성 on 1/5/25.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-protocol TouchFeedback {
+protocol ORBTouchFeedback {
     
     var animator: UIViewPropertyAnimator { get }
     
