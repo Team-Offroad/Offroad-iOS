ca602de63113be7da557b658a2b4fc9d9027e4a4
Minsung Kim <mskim4048@naver.com>
2025-01-05 20:26:25 +0900

    [Add/#423] ShrinkableCollectionViewCell 추가

---
+++ Offroad-iOS/Offroad-iOS/Global/Components/CustomView/ShrinkableCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ShrinkableCollectionViewCell.swift
+//  Offroad-iOS
+//
+//  Created by 김민성 on 1/5/25.
+//
+
+import UIKit
+
+class ShrinkableCollectionViewCell: UICollectionViewCell {
+    
+}
