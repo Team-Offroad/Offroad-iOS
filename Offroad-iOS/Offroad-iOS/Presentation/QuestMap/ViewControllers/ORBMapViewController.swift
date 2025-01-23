32ab928770f9028c7cd6c6821e01a2d93212d37c
Minsung Kim <mskim4048@naver.com>
2025-01-23 12:49:45 +0900

    [Chore/#431] setCameraPosition에 주석 추가

--- Offroad-iOS/Offroad-iOS/Presentation/QuestMap/ViewControllers/ORBMapViewController.swift
+++ Offroad-iOS/Offroad-iOS/Presentation/QuestMap/ViewControllers/ORBMapViewController.swift
@@ -342,6 +342,13 @@ extension ORBMapViewController {
         }
     }
     
+    /// 새로운 NMFCameraPosition을 지도의 카메라를 설정하는 함수
+    /// - Parameters:
+    ///   - newPosition: 변경될 지도의 새 position
+    ///   - animationDuration: 애니메이션 시간. 0으로 설정할 경우, 애니메이션 없이 바로 이동. 기본값은 NAVER Map iOS SDK에서 설정하는 0.2
+    ///   - animationCurve: 지도가 움직일 때의 애니메이션 종류. NMFCameraUpdateAnimation 타입.
+    ///   - reason: 카메라 이동에 사용될 `NMFCameraUpdate` 타입의 `reason` 속성에 할당할 값. 기본값은 `NMFMapChangedByDeveloper`이며, -1에 해당. (개발자가 API를 호출해 카메라가 움직였음을 의미)
+    ///   - completion: 카메라 이동이 완료되었을 때 호출되는 콜백 블록. 애니메이션이 있으면 완전히 끝난 후에 호출됩니다. `Bool` 타입의 매개변수는 카메라 이동이 완료되기 전에 다른 카메라 이동이 호출되거나 사용자가 제스처로 지도를 조작한 경우 `true`입니다.
     private func setCameraPosition(to newPosition: NMFCameraPosition,
                                    // NAVER Map iOS SDK에서 지정하는 기본값이 0.2
                                    animationDuration: TimeInterval = 0.2,
