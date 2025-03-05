80793e89df94ea2ef9b36feb19cbd199114b48cd
JoHyerin <gpfls468@gmail.com>
2025-03-05 17:04:02 +0900

    [Feat/#472] 가이드 뷰 UI 구현 마무리

--- Offroad-iOS/Offroad-iOS/Presentation/Diary/View/DiaryGuideView.swift
+++ Offroad-iOS/Offroad-iOS/Presentation/Diary/View/DiaryGuideView.swift
@@ -15,6 +15,8 @@ final class DiaryGuideView: UIView {
     //MARK: - UI Properties
     
     let closeButton = UIButton()
+    let guideCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout())
+    let pageControl = UIPageControl()
     let nextButton = ShrinkableButton()
     
     // MARK: - Life Cycle
@@ -43,6 +45,32 @@ extension DiaryGuideView {
             $0.setImage(.iconCloseWhite, for: .normal)
         }
         
+        guideCollectionView.do {
+            let flowLayout = UICollectionViewFlowLayout()
+            flowLayout.scrollDirection = .horizontal
+            flowLayout.minimumLineSpacing = 0
+            flowLayout.minimumInteritemSpacing = 0
+            
+            $0.collectionViewLayout = flowLayout
+            $0.isPagingEnabled = true
+            $0.showsHorizontalScrollIndicator = false
+            $0.backgroundColor = .clear
+            
+            $0.register(GuideCollectionViewCell.self, forCellWithReuseIdentifier: GuideCollectionViewCell.className)
+        }
+        
+        pageControl.do {
+            $0.currentPage = 0
+            $0.pageIndicatorTintColor = UIColor.primary(.white).withAlphaComponent(0.4)
+            $0.currentPageIndicatorTintColor = UIColor.primary(.white)
+            $0.isUserInteractionEnabled = false
+            $0.numberOfPages = 2
+            
+            for i in 0..<2 {
+                $0.setIndicatorImage(UIImage(resource: .imgCurrentIndicator), forPage: i)
+            }
+        }
+        
         nextButton.do {
             $0.setTitle("다음", for: .normal)
             $0.setTitleColor(.main(.main1), for: .normal)
@@ -56,6 +84,8 @@ extension DiaryGuideView {
     private func setupHierarchy() {
         addSubviews(
             closeButton,
+            guideCollectionView,
+            pageControl,
             nextButton
         )
     }
@@ -65,6 +95,19 @@ extension DiaryGuideView {
             $0.top.equalTo(safeAreaLayoutGuide).inset(4)
             $0.trailing.equalToSuperview().inset(14)
             $0.size.equalTo(44)
+        }
+        
+        guideCollectionView.snp.makeConstraints {
+            $0.top.greaterThanOrEqualTo(safeAreaLayoutGuide).inset(100)
+            $0.top.lessThanOrEqualTo(safeAreaLayoutGuide).inset(150)
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(418).priority(.low)
+        }
+        
+        pageControl.snp.makeConstraints {
+            $0.top.equalTo(guideCollectionView.snp.bottom).offset(54)
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalTo(nextButton.snp.top).offset(-80)
         }
         
         nextButton.snp.makeConstraints {
