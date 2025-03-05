3c7e045b6808b72d526ca4439fcc3ad0ab314982
JoHyerin <gpfls468@gmail.com>
2025-03-05 17:04:32 +0900

    [Feat/#472] CollectionView 바인딩, PageControl 로직 구현

--- Offroad-iOS/Offroad-iOS/Presentation/Diary/ViewController/DiaryGuideViewController.swift
+++ Offroad-iOS/Offroad-iOS/Presentation/Diary/ViewController/DiaryGuideViewController.swift
@@ -13,6 +13,12 @@ final class DiaryGuideViewController: UIViewController {
     
     private let rootView = DiaryGuideView()
     
+    private let guideCharacterData: [UIImage] = [.imgCharacterDiaryGuide, .imgCharacterDiaryGuide]
+    private let guideDescription1 = ["오브와 대화를 나누거나\n함께 탐험을 떠나면,\n매일 기록을 모아 오브가 일기를 써요.", "이건 기억빛이에요."]
+    private let guideDescription2 = ["일기를 받기 위해선\n오브와 충분한 시간을 보내야해요.", "그 날의 기억에 따라\n다른 색으로 칠해져요.\n오늘은 어떤 색의 하루였나요?"]
+    
+    private var currentPage = 0
+
     // MARK: - Life Cycle
     
     override func loadView() {
@@ -23,6 +29,7 @@ final class DiaryGuideViewController: UIViewController {
         super.viewDidLoad()
         
         setupTarget()
+        setupCollectionView()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -37,6 +44,11 @@ private extension DiaryGuideViewController {
     func setupTarget() {
         rootView.closeButton.addTarget(self, action: #selector(closeButtonTapped), for: .touchUpInside)
     }
+    
+    func setupCollectionView() {
+        rootView.guideCollectionView.delegate = self
+        rootView.guideCollectionView.dataSource = self
+    }
 }
     
 @objc private extension DiaryGuideViewController {
@@ -45,5 +57,32 @@ private extension DiaryGuideViewController {
     
     func closeButtonTapped() {
         dismiss(animated: false)
+    }
+}
+
+extension DiaryGuideViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return guideCharacterData.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: GuideCollectionViewCell.className, for: indexPath) as? GuideCollectionViewCell else { return UICollectionViewCell() }
+        cell.configureCell(image: guideCharacterData[indexPath.item], description1: guideDescription1[indexPath.item], description2: guideDescription2[indexPath.item])
+        
+        return cell
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        currentPage = scrollView.contentOffset.x < view.frame.width ? 0 : 1
+        
+        rootView.pageControl.currentPage = currentPage
+        rootView.nextButton.setTitle(currentPage == 0 ? "다음" : "확인", for: .normal)
+    }
+}
+
+
+extension DiaryGuideViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: rootView.guideCollectionView.bounds.width, height: rootView.guideCollectionView.bounds.height)
     }
 }
