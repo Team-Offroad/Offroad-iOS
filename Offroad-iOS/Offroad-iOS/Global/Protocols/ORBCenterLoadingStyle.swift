14adc6e83c3c0c092358e728f4a40e7598291901
Minsung Kim <mskim4048@naver.com>
2025-04-02 22:44:53 +0900

    [Chore/#502] ORBCenterLoading 메서드 이름 일부 수정

--- Offroad-iOS/Offroad-iOS/Global/Protocols/ORBCenterLoadingStyle.swift
+++ Offroad-iOS/Offroad-iOS/Global/Protocols/ORBCenterLoadingStyle.swift
@@ -13,8 +13,8 @@ import SnapKit
 /// 어떤 뷰의 중앙에 로딩 화면을 띄우고자 할 경우, 해당 뷰에서 이 프로토콜을 채택하세요.
 public protocol ORBCenterLoadingStyle: UIView {
     
-    func startLoading(withoutShading: Bool)
-    func stopLoading()
+    func startCenterLoading(withoutShading: Bool)
+    func stopCenterLoading()
     
 }
 
@@ -24,7 +24,7 @@ public extension ORBCenterLoadingStyle {
     /// - Parameter withoutShading: 로딩 시 배경을 어둡게 할 지 여부. true일 경우 어두워지지 않음. false인 경우 어두워짐.
     ///
     /// 만약 화면 중앙 로딩중인 경우, 추가적인 로딩 뷰를 띄우지 않으며, withoutShading 값에 따라 업데이트할 뿐.
-    func startLoading(withoutShading: Bool) {
+    func startCenterLoading(withoutShading: Bool) {
         for subview in subviews {
             if let loadingView = subview as? ORBLoadingView {
                 subview.backgroundColor = withoutShading ? .clear : .black.withAlphaComponent(0.55)
@@ -45,7 +45,7 @@ public extension ORBCenterLoadingStyle {
     }
     
     /// 화면 중앙 로딩중일 경우, 로딩을 종료.
-    func stopLoading() {
+    func stopCenterLoading() {
         subviews.forEach { view in
             if let loadingView = view as? ORBLoadingView { loadingView.removeFromSuperview() }
         }
