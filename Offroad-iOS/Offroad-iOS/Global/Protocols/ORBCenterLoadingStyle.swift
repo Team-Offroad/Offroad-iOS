d3784f8217c938a0915eef432f840d8dd5cb229b
Minsung Kim <mskim4048@naver.com>
2025-04-02 22:39:08 +0900

    [Add/#502] ORBCenterLoadingStyle, ORBScrollLoadingStyle 추가

---
+++ Offroad-iOS/Offroad-iOS/Global/Protocols/ORBCenterLoadingStyle.swift
@@ -0,0 +1,77 @@
+//
+//  ORBCenterLoadingStyle.swift
+//  Offroad-iOS
+//
+//  Created by 김민성 on 4/2/25.
+//
+
+import UIKit
+
+import Lottie
+import SnapKit
+
+/// 어떤 뷰의 중앙에 로딩 화면을 띄우고자 할 경우, 해당 뷰에서 이 프로토콜을 채택하세요.
+public protocol ORBCenterLoadingStyle: UIView {
+    
+    func startLoading(withoutShading: Bool)
+    func stopLoading()
+    
+}
+
+public extension ORBCenterLoadingStyle {
+    
+    /// 화면 중앙 로딩 시작.
+    /// - Parameter withoutShading: 로딩 시 배경을 어둡게 할 지 여부. true일 경우 어두워지지 않음. false인 경우 어두워짐.
+    ///
+    /// 만약 화면 중앙 로딩중인 경우, 추가적인 로딩 뷰를 띄우지 않으며, withoutShading 값에 따라 업데이트할 뿐.
+    func startLoading(withoutShading: Bool) {
+        for subview in subviews {
+            if let loadingView = subview as? ORBLoadingView {
+                subview.backgroundColor = withoutShading ? .clear : .black.withAlphaComponent(0.55)
+                return
+            }
+        }
+        
+        let loadingView = ORBLoadingView()
+        if withoutShading { loadingView.backgroundColor = .black.withAlphaComponent(0.55) }
+        
+        addSubview(loadingView)
+        
+        if let scrollView = self as? UIScrollView {
+            loadingView.snp.makeConstraints { $0.edges.equalTo(scrollView.frameLayoutGuide) }
+        } else {
+            loadingView.snp.makeConstraints { $0.edges.equalToSuperview() }
+        }
+    }
+    
+    /// 화면 중앙 로딩중일 경우, 로딩을 종료.
+    func stopLoading() {
+        subviews.forEach { view in
+            if let loadingView = view as? ORBLoadingView { loadingView.removeFromSuperview() }
+        }
+    }
+    
+}
+
+/// 화면 중앙 로딩에 사용될 로딩 뷰. 
+fileprivate final class ORBLoadingView: UIView {
+    
+    let lottie = Lottie.LottieAnimationView(name: "loading2")
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        lottie.loopMode = .loop
+        lottie.play()
+        addSubview(lottie)
+        lottie.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.size.equalTo(150)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
---
+++ Offroad-iOS/Offroad-iOS/Global/Protocols/ORBScrollLoadingStyle.swift
@@ -0,0 +1,88 @@
+//
+//  ORBScrollLoadingStyle.swift
+//  Offroad-iOS
+//
+//  Created by 김민성 on 4/2/25.
+//
+
+import UIKit
+
+import Lottie
+import SnapKit
+
+/// 무한스크롤 시 scroll view 컨텐츠 아래에 로딩 화면을 띄우고자 할 경우, 해당 scroll view에서 이 프로토콜을 채택하세요.
+public protocol ORBScrollLoadingStyle: UIScrollView {
+    
+    /// scroll view의 loading lottie가 content로부터 떨어져 있는 값.
+    var scrollLoadingLottieOffset: CGFloat { get }
+    
+    /// loading lottie의 한 변의 길이 (scroll loading lottie는 정방형 비율)
+    var scrollLoadingLottieSideLength: CGFloat { get }
+    
+    /// 로딩 시 스크롤 방향으로 추가될 영역의 길이.
+    var additionalContentInset: CGFloat { get }
+    
+    func startBottomScrollLoading()
+    func stopBottomScrollLoading()
+    
+}
+
+public extension ORBScrollLoadingStyle {
+    
+    var scrollLoadingLottieOffset: CGFloat { 18 }
+    var scrollLoadingLottieSideLength: CGFloat { 38 }
+    var additionalContentInset: CGFloat { scrollLoadingLottieSideLength + (scrollLoadingLottieOffset * 2) }
+    
+    /// scroll view 아래쪽의 무한 스크롤 로딩 화면을 띄웁니다.
+    func startBottomScrollLoading() {
+        let scrollLoadingView = ORBScrollLoadingView()
+        contentInset.bottom += additionalContentInset
+        addSubview(scrollLoadingView)
+        layoutIfNeeded()
+        scrollLoadingView.frame = .init(
+            x: bounds.width/2 - scrollLoadingLottieSideLength/2,
+            y: contentSize.height + scrollLoadingLottieOffset,
+            width: scrollLoadingLottieSideLength,
+            height: scrollLoadingLottieSideLength
+        )
+    }
+    
+    /// scroll view 컨텐츠 아래쪽에 무한 스크롤 로딩이 있다면 로딩을 멈추고 추가된 영역을 없앱니다.
+    func stopBottomScrollLoading() {
+        var isScrollLoading: Bool = false
+        subviews.forEach { view in
+            if let scrollLoadingView = view as? ORBScrollLoadingView {
+                scrollLoadingView.removeFromSuperview()
+                isScrollLoading = true
+            }
+        }
+        if isScrollLoading {
+            contentInset.bottom -= additionalContentInset
+        }
+    }
+    
+}
+
+
+/// ORBScrollLoadinStyle에 사용될 lottie를 포함하는 뷰. 이 뷰의 크기가 곧 로티의 크기에 해당.
+fileprivate final class ORBScrollLoadingView: UIView {
+    
+    let lottie = Lottie.LottieAnimationView(name: "loading1")
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        isUserInteractionEnabled = false
+        lottie.loopMode = .loop
+        lottie.play()
+        addSubview(lottie)
+        lottie.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
