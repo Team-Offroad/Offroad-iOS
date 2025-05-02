c54b9dcf3a2312ee6e5474ce19487efecca24bef
Minsung Kim <mskim4048@naver.com>
2025-05-02 22:19:44 +0900

    [Add/#524] 채팅 뷰컨트롤러, 뷰 추가

---
+++ Offroad-iOS/Offroad-iOS/Presentation/ORBRecommendation/chat/ORBRecommendationChatView.swift
@@ -0,0 +1,14 @@
+//
+//  ORBRecommendationChatView.swift
+//  ORB_Dev
+//
+//  Created by 김민성 on 5/2/25.
+//
+
+import UIKit
+
+final class ORBRecommendationChatView: UIView {
+    
+    
+    
+}
---
+++ Offroad-iOS/Offroad-iOS/Presentation/ORBRecommendation/chat/ORBRecommendationChatViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ORBRecommendationChatViewController.swift
+//  ORB_Dev
+//
+//  Created by 김민성 on 5/2/25.
+//
+
+import UIKit
+
+final class ORBRecommendationChatViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+}
