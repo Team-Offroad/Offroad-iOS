b286ff3ca4b578a080d4f53c8ea3e77fd5aebf7d
codeJiwon <loopy0107@sookmyung.ac.kr>
2025-05-13 20:45:07 +0900

    [Feat/#534] CourseQuestPlace model 파일 생성

---
+++ Offroad-iOS/Offroad-iOS/Presentation/CourseQuest/Models/CourseQuestPlace.swift
@@ -0,0 +1,18 @@
+//
+//  CourseQuestPlace.swift
+//  Offroad-iOS
+//
+//  Created by  정지원 on 5/13/25.
+//
+#if DevTarget
+import Foundation
+
+struct CourseQuestPlace {
+    let id: UUID
+    let imageName: String
+    let type: String
+    let name: String
+    let address: String
+    var isVisited: Bool
+}
+#endif
