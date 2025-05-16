08f74548edf376f8041599cf2af6edc06974f98b
Minsung Kim <mskim4048@naver.com>
2025-05-16 19:22:41 +0900

    [Refactor/#540] ORBSegmentedControl 리팩토링
    - 역할별로 확장된 영역 분리
    - 주석 추가

--- Offroad-iOS/Offroad-iOS/Global/Components/SegmentedControl/ORBSegmentedControl.swift
+++ Offroad-iOS/Offroad-iOS/Global/Components/SegmentedControl/ORBSegmentedControl.swift
@@ -18,16 +18,14 @@ final class ORBSegmentedControl: UIView {
     
     //MARK: - Properties
     
-    let animator = UIViewPropertyAnimator(duration: 0.3, dampingRatio: 1)
-    weak var delegate: ORBSegmentedControlDelegate? = nil
-    var titles: [String]
-    var isLayoutDone: Bool = false
+    private let underbarAnimator = UIViewPropertyAnimator(duration: 0, dampingRatio: 1)
+    private lazy var underbarLeadingConstraint = underbar.leadingAnchor.constraint(equalTo: leadingAnchor)
+    private var segmentWidth: CGFloat { frame.width / CGFloat(titles.count) }
+    var segmentsCount: Int { stackView.arrangedSubviews.count }
     
+    private(set) var titles: [String]
     private(set) var selectedIndex: Int = 0
-    
-    // underbar의 leading, trailing anchor의 제약을 담는 변수. 여기서 할당하는 값은 초깃값을 위한 값일 뿐이므로 의미 없음.
-    lazy var underbarLeadingConstraint = underbar.leadingAnchor.constraint(equalTo: leadingAnchor)
-    lazy var underbarTrailingConstraint = underbar.trailingAnchor.constraint(equalTo: trailingAnchor)
+    weak var delegate: ORBSegmentedControlDelegate? = nil
     
     //MARK: - UI Properties
     
@@ -41,56 +39,26 @@ final class ORBSegmentedControl: UIView {
         let buttonsArray = titles.enumerated().map { (index, title) in
             ORBSegmentedControlButton(title: title, tag: index)
         }
-        self.stackView = UIStackView(arrangedSubviews: buttonsArray)
+        stackView = UIStackView(arrangedSubviews: buttonsArray)
         super.init(frame: .zero)
         
         setupStyle()
         setupHierarchy()
         setupLayout()
         setButtonsTarget()
+        updateButtonSelectedState(selectedIndex: 0)
     }
     
     required init(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        
-        isLayoutDone = true
-        updateUnderbarPosition(to: selectedIndex)
-        selectSegment(index: selectedIndex)
-    }
-    
 }
 
-extension ORBSegmentedControl {
+// Initial Settings
+private extension ORBSegmentedControl {
     
-    //MARK: @objc Func
-    
-    @objc private func segmentDidSelected(sender: UIButton) {
-        guard sender.tag != selectedIndex else { return }
-        delegate?.segmentedControlWillSelect?(segmentedControl: self, selectedIndex: sender.tag)
-        selectSegment(index: sender.tag)
-        delegate?.segmentedControlDidSelect?(segmentedControl: self, selectedIndex: sender.tag)
-    }
-    
-    //MARK: - Layout Func
-    
-    private func setupLayout() {
-        stackView.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
-        }
-        
-        underbar.snp.makeConstraints { make in
-            make.bottom.equalToSuperview()
-            make.height.equalTo(3)
-        }
-    }
-    
-    //MARK: - Private Func
-    
-    private func setupStyle() {
+    func setupStyle() {
         stackView.do { stackView in
             stackView.axis = .horizontal
             stackView.distribution = .fillEqually
@@ -99,49 +67,108 @@ extension ORBSegmentedControl {
         underbar.backgroundColor = .sub(.sub)
     }
     
-    private func setupHierarchy() {
+    func setupHierarchy() {
         addSubviews(stackView, underbar)
     }
     
-    private func setButtonsTarget() {
+    func setupLayout() {
+        stackView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        underbarLeadingConstraint.isActive = true
+        underbar.snp.makeConstraints { make in
+            make.bottom.equalToSuperview()
+            make.height.equalTo(3)
+            make.width.equalTo(self.snp.width).dividedBy(titles.count)
+        }
+    }
+    
+    func setButtonsTarget() {
         stackView.arrangedSubviews.forEach { view in
             guard let button = view as? ORBSegmentedControlButton else { return }
-            button.addTarget(self, action: #selector(segmentDidSelected(sender:)), for: .touchUpInside)
+            button.addTarget(self, action: #selector(buttonDidSelected(sender:)), for: .touchUpInside)
         }
     }
     
-    private func updateUnderbarPosition(to index: Int) {
-        let selectedButton = stackView.arrangedSubviews[index]
-        animator.addAnimations { [unowned self] in
-            self.underbarLeadingConstraint.isActive = false
-            self.underbarTrailingConstraint.isActive = false
-            
-            self.underbarLeadingConstraint = underbar.leadingAnchor.constraint(equalTo: selectedButton.leadingAnchor)
-            self.underbarTrailingConstraint = underbar.trailingAnchor.constraint(equalTo: selectedButton.trailingAnchor)
-            self.underbarLeadingConstraint.isActive = true
-            self.underbarTrailingConstraint.isActive = true
-            self.layoutIfNeeded()
-        }
-        animator.startAnimation()
+    //MARK: @objc Func
+    
+    @objc func buttonDidSelected(sender: UIButton) {
+        selectSegment(index: sender.tag)
     }
     
-    private func updateSegmentState(selectedIndex: Int) {
+}
+
+// underbar 위치 설정 관련 private 함수들.
+private extension ORBSegmentedControl {
+    
+    /// 화면상에서 특정한 거리만큼 underbar의 위치를 업데이트하는 함수. 선택한 offset 값에 맞게 애니메이션이 적용됨.
+    /// - Parameters:
+    ///   - offset: underbar가 leading edge로부터 떨어져 있을 값.
+    ///   - completion: 애니메이션이 완료된 후 호출되는 콜백 함수.
+    func animateUnderbarPosition(offset: CGFloat, completion: ((UIViewAnimatingPosition) -> Void)? = nil) {
+        // 손쉬운 사용>동작>동작 줄이기 항목을 킨 경우 애니메이션 미적용함.
+        guard !UIAccessibility.isReduceMotionEnabled else {
+            underbarLeadingConstraint.constant = offset
+            layoutIfNeeded()
+            return
+        }
+        
+        underbarAnimator.stopAnimation(true)
+        underbarAnimator.addAnimations { [weak self] in
+            self?.underbarLeadingConstraint.constant = offset
+            self?.layoutIfNeeded()
+        }
+        underbarAnimator.addCompletion { completion?($0) }
+        underbarAnimator.startAnimation()
+    }
+    
+    /// 특정한 index에 underbar가 위치하도록 underbar의 위치를 업데이트하는 함수. 선택한 index에 맞는 segment로 underbar 가 이동하는 애니메이션이 적용됨.
+    /// - Parameters:
+    ///   - index: underbar가 위치할 segment의 index
+    ///   - completion: 애니메이션이 완료된 후 호출되는 콜백 함수.
+    func animateUnderbarPosition(to index: Int, completion: ((UIViewAnimatingPosition) -> Void)? = nil) {
+        let offset = CGFloat(index) * segmentWidth
+        animateUnderbarPosition(offset: offset, completion: { completion?($0) })
+    }
+    
+    /// 특정 segment가 선택됨에 따라 버튼들의 selected 상태를 업데이트 하는 함수.
+    /// - Parameter selectedIndex: 선택된 segment의 index
+    func updateButtonSelectedState(selectedIndex: Int) {
         stackView.arrangedSubviews.forEach { view in
             let button = view as! UIButton
             button.isSelected = (button.tag == selectedIndex)
         }
     }
     
-    //MARK: - Func
+}
+
+// segment 선택, underbar 위치 설정 관련 internal 함수들.
+extension ORBSegmentedControl {
     
-    func selectSegment(index: Int) {
-        guard index >= 0 && index < stackView.arrangedSubviews.count else { return }
+    /// 특정 segment를 선택하는 동작. selected segment가 바뀔 경우 선택 동작의 직전과 직후에 `delegate` 메서드가 호출됨.
+    /// - Parameters:
+    ///   - index: 선택할 segment
+    ///   - completion: underbar가 이동하는 애니메이션이 완료된 후 호출되는 콜백 함수.
+    func selectSegment(index: Int, completion: ((UIViewAnimatingPosition) -> Void)? = nil) {
+        guard index != selectedIndex else { return }
+        guard 0 <= index && index < segmentsCount else { return }
+        delegate?.segmentedControlWillSelect?(segmentedControl: self, selectedIndex: index)
         selectedIndex = index
-        updateSegmentState(selectedIndex: index)
-        guard isLayoutDone else { return }
-        updateUnderbarPosition(to: index)
+        updateButtonSelectedState(selectedIndex: index)
+        animateUnderbarPosition(to: index) { completion?($0) }
+        delegate?.segmentedControlDidSelect?(segmentedControl: self, selectedIndex: index)
     }
     
+}
+
+// Buttons title 설정 관련 함수들
+extension ORBSegmentedControl {
+    
+    /// 특정 index의 segment 이름을 바꾸는 함수.
+    /// - Parameters:
+    ///   - index: 바꿀 segment의 index
+    ///   - newTitle: 바꾸고자 하는 segment에 적용할 새 title
     func changeSegmentTitle(at index: Int, to newTitle: String) {
         guard let button = stackView.arrangedSubviews[index] as? ORBSegmentedControlButton else { return }
         button.setTitle(newTitle, for: .normal)
