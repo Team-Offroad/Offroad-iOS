fe2ee4e7573403380c365a8f045c10d5c650b787
Minsung Kim <mskim4048@naver.com>
2025-05-29 18:05:02 +0900

    [Chore/#553] PlaceService에서 장소 목록 가져오는 함수 개선. async/await 적용.

--- Offroad-iOS/Offroad-iOS/Network/Place/PlaceService.swift
+++ Offroad-iOS/Offroad-iOS/Network/Place/PlaceService.swift
@@ -5,6 +5,7 @@
 //  Created by 조혜린 on 7/15/24.
 //
 
+import CoreLocation
 import Foundation
 
 import Moya
@@ -21,12 +22,10 @@ protocol RegisteredPlaceServiceProtocol {
         completion: @escaping (NetworkResult<RegisteredPlaceResponseDTO>) -> ()
     )
     func getRegisteredListPlaces(
-        latitude: Double,
-        longitude: Double,
+        at coordinate: CLLocationCoordinate2D,
         limit: Int,
-        cursorDistance: Double?,
-        completion: @escaping (NetworkResult<RegisteredPlaceResponseDTO>) -> ()
-    )
+        cursorDistance: Double?
+    ) async throws -> [PlaceModel]
 }
 
 final class RegisteredPlaceService: BaseService, RegisteredPlaceServiceProtocol {
@@ -94,39 +93,85 @@ final class RegisteredPlaceService: BaseService, RegisteredPlaceServiceProtocol 
         }
     }
     
-    func getRegisteredListPlaces(
+    // 아직 사용되지 않는 함수임.
+    func getRegisteredMapPlaces(
         latitude: Double,
         longitude: Double,
+        limit: Int
+    ) async throws -> [PlaceModel] {
+        let api = PlaceAPI.getRegisteredMapPlaces(
+            latitude: latitude,
+            longitude: longitude,
+            limit: limit
+        )
+        return try await fetchPlaces(using: api)
+    }
+    
+    func getRegisteredListPlaces(
+        at coordinate: CLLocationCoordinate2D,
         limit: Int,
-        cursorDistance: Double? = nil,
-        completion: @escaping (NetworkResult<RegisteredPlaceResponseDTO>) -> ()
-    ) {
-        provider.request(
-            .getRegisteredListPlaces(
-                latitude: latitude,
-                longitude: longitude,
-                limit: limit,
-                cursorDistance: cursorDistance
-            )
-        ) { result in
-            switch result {
-            case .success(let response):
-                let networkResult: NetworkResult<RegisteredPlaceResponseDTO> = self.fetchNetworkResult(
-                    statusCode: response.statusCode,
-                    data: response.data
-                )
-                completion(networkResult)
-            case .failure(let error):
-                guard let response = error.response else {
-                    completion(.networkFail(nil))
-                    return
+        cursorDistance: Double? = nil
+    ) async throws -> [PlaceModel] {
+        let api = PlaceAPI.getRegisteredListPlaces(
+            latitude: coordinate.latitude,
+            longitude: coordinate.longitude,
+            limit: limit,
+            cursorDistance: cursorDistance
+        )
+        return try await fetchPlaces(using: api)
+    }
+    
+    /// 주어진 api로 오브에 등록된 장소 목록을 서버에 요청해 비동기적으로 반환하는 함수.
+    /// - Parameter api: 서버 요청에 사용할 API. `PlaceAPI` 타입.(`Moya`의 `TargetType` 프로토콜을 준수함.)
+    /// - Returns: 요청한 장소 목록.`[PlaceModel]` 타입.
+    private func fetchPlaces(using api: PlaceAPI) async throws -> [PlaceModel] {
+        try await withCheckedThrowingContinuation { continuation in
+            provider.request(api) { result in
+                switch result {
+                case .success(let response):
+                    // http 상태 코드 확인.
+                    let statusCode = response.statusCode
+                    guard (200...299).contains(statusCode) else {
+                        continuation.resume(throwing: NetworkResultError.httpError(statusCode: statusCode))
+                        return
+                    }
+                    
+                    // 지정된 DTO 형식으로 디코딩.
+                    guard let decodedDTO = try? JSONDecoder().decode(
+                        RegisteredPlaceResponseDTO.self,
+                        from: response.data
+                    ) else {
+                        continuation.resume(throwing: NetworkResultError.decodingFailed)
+                        return
+                    }
+                    
+                    // 디코딩된 데이터를 최종적으로 반화하는 타입으로 변환.
+                    do {
+                        let decodedDTOList = decodedDTO.data.places
+                        let places = try decodedDTOList.map { try PlaceModel($0) }
+                        continuation.resume(returning: places)
+                    } catch {
+                        continuation.resume(throwing: error)
+                    }
+                case .failure(let error):
+                    switch error {
+                    case .underlying(let underlyingError, _):
+                        if let urlError = underlyingError as? URLError {
+                            switch urlError.code {
+                            case .notConnectedToInternet, .cannotConnectToHost:
+                                continuation.resume(throwing: NetworkResultError.networkFailed)
+                            case .timedOut:
+                                continuation.resume(throwing: NetworkResultError.networkTimeout)
+                            default:
+                                continuation.resume(throwing: error)
+                            }
+                        }
+                    default:
+                        continuation.resume(throwing: error)
+                    }
                 }
-                let networkResult: NetworkResult<RegisteredPlaceResponseDTO> = self.fetchNetworkResult(
-                    statusCode: response.statusCode,
-                    data: response.data
-                )
-                completion(networkResult)
             }
         }
     }
+    
 }
