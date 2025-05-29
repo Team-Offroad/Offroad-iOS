f125e420ff2c00c4edfa9d8e893a6079cc664c38
Minsung Kim <mskim4048@naver.com>
2025-05-29 13:01:38 +0900

    [Chore/#550] printLog 메서드 동작 및 주석 수정

--- Offroad-iOS/Offroad-iOS/Global/Components/Overlay/LogPrinter/PrintLogInDevApp.swift
+++ Offroad-iOS/Offroad-iOS/Global/Components/Overlay/LogPrinter/PrintLogInDevApp.swift
@@ -5,10 +5,13 @@
 //  Created by 김민성 on 2/1/25.
 //
 
-/*
- 개발용 앱에 한하여 원하는 문구를 앱 내에 띄워줄 수 있습니다.
- */
+/// 원하는 문구를 개발용 앱 내의 로그 콘솔에 띄우는 동작.
+/// - Parameter log: 개발용 앱의 콘솔에 표시할 로그 문구
+///
+/// Xcode의 콘솔에 print하는 동작도 포함합니다. (Xcode 콘솔에 띄우기 위해 추가로 print 호출할 필요 없음.)
+/// 배포용 앱에서는 print 문과 완전히 같은 동작입니다.
 func printLog(_ log: String) {
+    print(log)
     #if DevTarget
     LogPrinterManager.shared.printLog(log)
     #else
