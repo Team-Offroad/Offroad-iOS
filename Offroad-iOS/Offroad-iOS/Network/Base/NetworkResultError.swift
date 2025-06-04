3d37370a9b2177f29b3901df440f833a040da697
Minsung Kim <mskim4048@naver.com>
2025-06-04 12:41:14 +0900

    [Chore/#553] NetworkResultError 일부 수정 및 케이스 추가

--- Offroad-iOS/Offroad-iOS/Network/Base/NetworkResultError.swift
+++ Offroad-iOS/Offroad-iOS/Network/Base/NetworkResultError.swift
@@ -7,28 +7,47 @@
 
 import Foundation
 
+import Alamofire
+
 /// 네트워크 통신 시 발생할 수 있는 에러
 enum NetworkResultError: LocalizedError {
     
-    /// `HTTP` 통신 후 상태 코드가 400~599인 경우 에러로 간주.
+    /// `HTTP` 통신 후 상태 코드가 200~299가 아닌 경우.
     case httpError(statusCode: Int)
     
     /// 응답값이 정해진 DTO로 디코딩되지 않음.
     case decodingFailed
     
-    /// 네트워크 통신 실패
-    case networkFailed
+    //===----------------------------------------------------------------------===//
+    // 여기부터 URLError
+    //===----------------------------------------------------------------------===//
     
-    /// 네트워크 Timtout
-    case networkTimeout
+    /// 네트워크 Timeout. `URLError.Code`가 `.timedOut`인 경우에 해당.
+    case timeOut
+    
+    /// 인터넷에 연결되어있지 않은 경우. `URLError.Code`가 `.notConnectedToInternet`인 경우에 해당.
+    case notConnectedToInternet
+    
+    /// 네트워크 취소됨. `URLError.Code`가 `.networkCancelled`인 경우에 해당.
+    case networkCancelled
+    
+    /// `URLError`이지만, `NetworkResultError`에서 다루지 않은 경우.
+    case unknownURLError(URLError)
+    
+    //===----------------------------------------------------------------------===//
+    // 여기까지 URLError
+    //===----------------------------------------------------------------------===//
+    
+    /// `NetworkResultError`에서 다루지 않은 나머지 모든 에러
+    case unknown((any Error)?)
     
     var errorDescription: String? {
         switch self {
         case .httpError(let statusCode):
             switch statusCode {
             case 200...299:
-                assertionFailure("상태 코드가 200번대인 경우는 에러가 아닙니다.")
-                return "상태 코드가 200번대인 경우는 에러가 아닙니다."
+                assertionFailure("상태 코드: \(statusCode). 200번대인 경우는 에러가 아닙니다.")
+                return "상태 코드: \(statusCode). 200번대인 경우는 에러가 아닙니다."
             case 400: return "400(Bad Request) 올바르지 않은 요청입니다."
             case 401: return "401(Unauthorized) 인증 실패(Unauthenticated)"
             case 403: return "403(Forbidden) 인가 실패(Unauthorized)"
@@ -39,10 +58,17 @@ enum NetworkResultError: LocalizedError {
             }
         case .decodingFailed:
             return "서버의 응답값(Body)을 지정한 DTO로 디코딩하는 데에 실패했습니다. 서버의 응답값 형식 또는 DTO를 확인하세요."
-        case .networkFailed:
+        case .timeOut:
+            return "네트워크 타임아웃."
+        case .notConnectedToInternet:
             return "네트워크 통신에 실패했습니다. 인터넷 연결 상태를 확인하세요."
-        case .networkTimeout:
-            return "네트워크 통신 타임아웃."
+        case .networkCancelled:
+            return "네트워크 요청이 취소되었습니다."
+        case .unknownURLError(let error):
+            return "NetworkResultError에서 처리하지 않은 URLError입니다: \(error)"
+        case .unknown(let error):
+            let errorMessage: String = "원인을 알 수 없는 네트워크 에러입니다."
+            return (error == nil) ? errorMessage : errorMessage + " 에러 코드: \(error!)"
         }
     }
     
